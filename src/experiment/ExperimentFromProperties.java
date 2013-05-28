488842afe55d4042423ff7bf13b562ef9baa3a41
Chris Wright <aca08cw@sheffield.ac.uk>
2013-05-28 16:38:01 +0100

    Adding framework for saving/loading parameters to Java Properties files

---
+++ src/experiment/ExperimentFromProperties.java
@@ -0,0 +1,23 @@
+/*
+ */
+package experiment;
+
+import java.io.File;
+import java.io.FileFilter;
+import org.javarunner.PropertiesRunner;
+
+/**
+ *
+ * @author chris
+ */
+public class ExperimentFromProperties {
+    
+    public static void main(String[] args) {
+        if (args == null || args[0] == null) {
+            throw new RuntimeException("ExperimentFromProperties requires a "
+                    + "uniqueid value to be provided (-Duniqueid=#)");
+        }
+        PropertiesRunner.startJVMFromProperties("ExperimentTasks/"+args[0]);
+    }
+    
+}
