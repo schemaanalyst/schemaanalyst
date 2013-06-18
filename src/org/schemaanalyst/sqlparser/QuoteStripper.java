f53a086307220a6a23a8c66cf919a90d0559d991
Phil McMinn <p.mcminn@sheffield.ac.uk>
2013-06-18 14:40:22 +0100

    Fixed some quoting issues

---
+++ src/org/schemaanalyst/sqlparser/QuoteStripper.java
@@ -0,0 +1,25 @@
+package org.schemaanalyst.sqlparser;
+
+import gudusoft.gsqlparser.nodes.TObjectName;
+
+public class QuoteStripper {
+
+	static final String[] quoteChars = {"\"", "'", "`"};
+	
+	static boolean isQuoted(String string) {
+		for (String quoteChar : quoteChars) {
+    		if (string.startsWith(quoteChar) && string.endsWith(quoteChar)) {
+    			return true;
+    		}
+		}
+		return false;
+	}
+	
+	static String stripQuotes(String string) {
+		return (isQuoted(string)) ? string.substring(1, string.length()-1) : string;
+	}
+	
+	static String stripQuotes(TObjectName name) {
+		return stripQuotes(name.toString());		
+	}	
+}
