d219ac228294e0735522408af9fcd31ff0fc7370
Phil McMinn <p.mcminn@sheffield.ac.uk>
2013-07-11 16:06:02 +0100

    Making GenerateData a tool for quick analysis

---
+++ src/org/schemaanalyst/tool/GenerateData.java
@@ -0,0 +1,22 @@
+package org.schemaanalyst.tool;
+
+public class GenerateData {
+
+	public static void GenerateData(String schemaName, String dataGenerationName) {
+		
+		/*
+		String fullSchemaClassName =  
+		Schema schema = 
+		
+		
+			DataGenerator generator = SchemaAnalyst.constructDataGenerator(
+					"alternatingvalue", schema, new ValueFactory());
+		
+			CoverageReport report = generator.generate();
+		
+			System.out.println(report);
+			
+		*/
+			
+	}
+}
