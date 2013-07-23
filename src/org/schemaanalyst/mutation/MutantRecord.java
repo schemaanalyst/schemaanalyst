989c403e8cb4574a298093a6701bbd32a20bced7
Chris Wright <aca08cw@sheffield.ac.uk>
2013-07-23 10:48:00 +0100

    Removing unused interface with misleading name

--- src/org/schemaanalyst/mutation/MutantRecord.java
+++ src/org/schemaanalyst/mutation/MutantRecord.java
@@ -4,7 +4,7 @@ package org.schemaanalyst.mutation;
  * This class represents the two-tuple (SQL Statement, Return Code). Used
  * directly inside of the SchemaAnalyst mutation analysis process.
  */
-public class MutantRecord extends SQLInsertRecord implements MutantRecordImpl {
+public class MutantRecord extends SQLInsertRecord {
 
     /**
      * The boolean flag indicating whether this statement killed the MUTANT
