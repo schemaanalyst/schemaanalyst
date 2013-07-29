8e9c960a0ddf35c9ec65c72622b9c6005ec42756
Phil McMinn <p.mcminn@sheffield.ac.uk>
2013-07-29 08:33:47 +0100

    Started to use JUnitParams to test objective functions; updated jar to 1.0.2

--- src/org/schemaanalyst/test/datageneration/search/objective/expression/TestRelationalExpressionObjectiveFunction.java
+++ src/org/schemaanalyst/test/datageneration/search/objective/expression/TestRelationalExpressionObjectiveFunction.java
@@ -1,89 +1,73 @@
 package org.schemaanalyst.test.datageneration.search.objective.expression;
 
+import static junitparams.JUnitParamsRunner.$;
+import static org.schemaanalyst.test.testutil.ObjectiveValueAssert.assertNonOptimal;
+import static org.schemaanalyst.test.testutil.ObjectiveValueAssert.assertOptimal;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.schemaanalyst.data.NumericValue;
+import org.schemaanalyst.data.Row;
+import org.schemaanalyst.datageneration.search.objective.ObjectiveFunction;
+import org.schemaanalyst.datageneration.search.objective.ObjectiveValue;
 import org.schemaanalyst.datageneration.search.objective.row.RelationalExpressionObjectiveFunction;
 import org.schemaanalyst.logic.RelationalOperator;
 import org.schemaanalyst.sqlrepresentation.expression.ConstantExpression;
 import org.schemaanalyst.sqlrepresentation.expression.RelationalExpression;
 import org.schemaanalyst.test.testutil.mock.MockRow;
 
-import static org.schemaanalyst.test.testutil.ObjectiveValueAssert.*;
-
+@RunWith(JUnitParamsRunner.class)
 public class TestRelationalExpressionObjectiveFunction {
-    
-    RelationalExpressionObjectiveFunction objFunSatisfy, objFunNotSatisfy;    
-    
-    @Test
-    public void expTrue_allowNullFalse() {
-        RelationalExpression exp 
-            = new RelationalExpression(
-                    new ConstantExpression(new NumericValue(1)),
-                    RelationalOperator.EQUALS, 
-                    new ConstantExpression(new NumericValue(1)));
-        
-        setupObjFuns(exp, false);        
-        assertOptimality(true);
-    }
 
-    @Test
-    public void expFalse_allowNullFalse() {
-        RelationalExpression exp 
-            = new RelationalExpression(
-                    new ConstantExpression(new NumericValue(2)),
-                    RelationalOperator.EQUALS, 
-                    new ConstantExpression(new NumericValue(1)));
-        
-        setupObjFuns(exp, false);        
-        assertOptimality(false);
-    }    
-    
-    @Test
-    public void expNull_allowNullFalse() {
-        RelationalExpression exp 
-            = new RelationalExpression(
-                    new ConstantExpression(null),
-                    RelationalOperator.EQUALS, 
-                    new ConstantExpression(new NumericValue(1)));
-        
-        setupObjFuns(exp, false);       
-        assertNullOptimality(false);
-    }    
-    
-    @Test
-    public void expNull_allowNullTrue() {
-        RelationalExpression exp 
-            = new RelationalExpression(
-                    new ConstantExpression(null),
-                    RelationalOperator.EQUALS, 
-                    new ConstantExpression(new NumericValue(1)));
-        
-        setupObjFuns(exp, true);                
-        assertNullOptimality(true);
-    }
+    RelationalExpression trueExp = new RelationalExpression(
+            new ConstantExpression(new NumericValue(1)),
+            RelationalOperator.EQUALS, 
+            new ConstantExpression(new NumericValue(1)));
 
-    private void setupObjFuns(RelationalExpression exp, boolean allowNull) {
-        objFunSatisfy = new RelationalExpressionObjectiveFunction(exp, true, allowNull);
-        objFunNotSatisfy = new RelationalExpressionObjectiveFunction(exp, false, allowNull);
-    }      
+    RelationalExpression falseExp = new RelationalExpression(
+            new ConstantExpression(new NumericValue(2)),
+            RelationalOperator.EQUALS, 
+            new ConstantExpression(new NumericValue(1)));
+
+    RelationalExpression nullExp = new RelationalExpression(
+            new ConstantExpression(null), RelationalOperator.EQUALS,
+            new ConstantExpression(new NumericValue(1)));
+
     
-    private void assertOptimality(boolean satisfyIsOptimal) {
-        if (satisfyIsOptimal) {
-            assertOptimal(objFunSatisfy.evaluate(new MockRow()));
-            assertNonOptimal(objFunNotSatisfy.evaluate(new MockRow()));
+    @SuppressWarnings("unused")
+    private Object[] testValues() {
+        return $(
+                $(trueExp, true, false, true),
+                $(trueExp, false, false, false),
+                $(trueExp, true, true, true),
+                $(trueExp, false, true, false),
+               
+                $(falseExp, false, false, true),
+                $(falseExp, true, false, false),
+                $(falseExp, false, true, true),
+                $(falseExp, true, true, false),
+                
+                $(nullExp, true, true, true),
+                $(nullExp, false, true, true),                
+                $(nullExp, true, false, false),
+                $(nullExp, false, false, false)
+        );
+    }    
+        
+    @Test
+    @Parameters(method = "testValues")    
+    public void testExpression(RelationalExpression exp, boolean goalIsToSatisfy, boolean allowNull, boolean optimal) {
+        ObjectiveFunction<Row> objFun = new RelationalExpressionObjectiveFunction(
+                exp, goalIsToSatisfy, allowNull);
+
+        ObjectiveValue objVal = objFun.evaluate(new MockRow()); 
+        
+        if (optimal) {
+            assertOptimal(objVal);            
         } else {
-            assertNonOptimal(objFunSatisfy.evaluate(new MockRow()));
-            assertOptimal(objFunNotSatisfy.evaluate(new MockRow()));            
-        }
+            assertNonOptimal(objVal);
+        }                
     }
-    
-    private void assertNullOptimality(boolean allowNull) {
-        if (allowNull) {
-            assertOptimal(objFunSatisfy.evaluate(new MockRow()));
-            assertOptimal(objFunNotSatisfy.evaluate(new MockRow()));
-        } else {
-            assertNonOptimal(objFunSatisfy.evaluate(new MockRow()));
-            assertNonOptimal(objFunNotSatisfy.evaluate(new MockRow()));            
-        }
-    }           
 }
