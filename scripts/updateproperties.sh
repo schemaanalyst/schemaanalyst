ff3467192191f2cc6da6b318f07dd5a40bab822f
Chris Wright <aca08cw@sheffield.ac.uk>
2013-07-30 14:16:53 +0100

    Simple script to update key-value pair in a properties file (using sed)

---
+++ scripts/updateproperties.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while getopts f:k:v: option
+do
+	case "${option}"
+		in
+		f) FILE=${OPTARG};;
+		k) KEY=${OPTARG};;
+		v) VALUE=${OPTARG};;
+	esac
+done
+
+if [ -z $FILE ] || [ -z $KEY ] || [ -z $VALUE ] ; then
+	echo "Update properties failed - requires -f FILE -k KEY -v VALUE"
+	exit 1
+fi
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   sed -i "s|\($KEY = \)\(.*\)|\1$VALUE|" $FILE
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   sed -i '' "s|\($KEY = \)\(.*\)|\1$VALUE|" $FILE
+fi
