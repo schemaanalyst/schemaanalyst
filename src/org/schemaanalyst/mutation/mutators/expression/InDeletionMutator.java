6f91bae5ca0ccdefc0f80b310a73196fcb98d2cd
Chris Wright <aca08cw@sheffield.ac.uk>
2013-08-06 20:20:33 +0100

    Refactored InDeletionMutator

--- src/org/schemaanalyst/mutation/mutators/expression/InDeletionMutator.java
+++ src/org/schemaanalyst/mutation/mutators/expression/InDeletionMutator.java
@@ -5,7 +5,6 @@ package org.schemaanalyst.mutation.mutators.expression;
 import java.util.ArrayList;
 import java.util.List;
 import org.schemaanalyst.sqlrepresentation.expression.Expression;
-import org.schemaanalyst.sqlrepresentation.expression.InExpression;
 import org.schemaanalyst.sqlrepresentation.expression.ListExpression;
 
 /**
@@ -15,16 +14,12 @@ import org.schemaanalyst.sqlrepresentation.expression.ListExpression;
 public class InDeletionMutator extends AbstractMutator {
 
     @Override
-    public void visit(InExpression expression) {
+    public void visit(ListExpression expression) {
         super.visit(expression);
-        Expression rhs = expression.getRHS();
-        if (rhs instanceof ListExpression) {
-            ListExpression listExpr = (ListExpression) rhs;
-            for (Expression subExpr : listExpr.getSubexpressions()) {
-                List<Expression> subExprs = new ArrayList<>(listExpr.getSubexpressions());
-                subExprs.remove(subExpr);
-                mutants.add(new InExpression(expression.getLHS(), new ListExpression(subExprs), expression.isNotIn()));
-            }
+        for (Expression subExpr : expression.getSubexpressions()) {
+            List<Expression> subExprs = new ArrayList<>(expression.getSubexpressions());
+            subExprs.remove(subExpr);
+            mutants.add(new ListExpression(subExprs));
         }
     }
 }
