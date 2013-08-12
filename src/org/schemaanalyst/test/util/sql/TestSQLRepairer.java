dbc27cd569396e4fa82b0ad9c29a4c2ed937374f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2013-08-12 15:48:15 -0400

    Updated the test suite; realistic example test case passes. More needed!

--- src/org/schemaanalyst/test/util/sql/TestSQLRepairer.java
+++ src/org/schemaanalyst/test/util/sql/TestSQLRepairer.java
@@ -27,4 +27,27 @@ public class TestSQLRepairer {
 				expectedExample, returnedExample);
 	}
 
+	@Test
+	public void spacesAreRemovedInsideOfSingleQuotes() {
+		String example = "\'hel lo\'";
+		String returnedExample = SQLRepairer.deleteSpaces(example); 
+
+		String expectedExample = "\'hello\'";
+		assertEquals("Expected string should be " + expectedExample,
+				expectedExample, returnedExample);
+	}
+
+	@Test
+	public void spacesAreRemovedInsideOfSingleQuotesRealExample() {
+		String example = "CREATE TABLE 'NUTR_DEF' ('Nutrr_no' TEXT, 'Nutrient name' TEXT, 'Flav_Class' TEXT, 'Unit' TEXT, 'Tagname' TEXT);";
+		String returnedExample = SQLRepairer.deleteSpacesInsideSingleQuotes(example); 
+
+		String expectedExample = "CREATE TABLE 'NUTR_DEF' ('Nutrr_no' TEXT, 'Nutrient_name' TEXT, 'Flav_Class' TEXT, 'Unit' TEXT, 'Tagname' TEXT);";
+
+		System.out.println("Returned example: " + returnedExample);
+
+		assertEquals("Expected string should be " + expectedExample,
+				expectedExample, returnedExample);
+	}
+
 }
