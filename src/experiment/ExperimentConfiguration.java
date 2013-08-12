bca6fad7fa8d13bc5a22f8381bba60abda44b439
Chris Wright <aca08cw@sheffield.ac.uk>
2013-08-12 16:33:35 +0100

    Removing old Experiment classes. Removed old DBMonster class. Removed old SchemaAnalyst class

--- src/experiment/ExperimentConfiguration.java
+++
@@ -1,9 +0,0 @@
-package experiment;
-
-import plume.Option;
-
-public class ExperimentConfiguration {
-
-    @Option("set the root of the project for running experiments")
-    public static String project = "";
-}
