fe95e6937c8187f1beae2df07ddd62764960d286
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2013-08-13 09:36:30 -0400

    This is the Flav DB repaired schema that was created in a fully automated way.

--- casestudies/schemas/Flav_R03-1-Repaired.sql
+++ casestudies/schemas/Flav_R03-1-Repaired.sql
@@ -1,8 +1,9 @@
+-- http://www.ars.usda.gov/Services/docs.htm?docid=6231
 CREATE TABLE DATA_SRC (DataSrc_ID TEXT, Authors TEXT, Title TEXT, Jorunal TEXT, Year TEXT, Volume TEXT, Issue TEXT, Start_Page TEXT, Emd_Page TEXT);
 CREATE TABLE DATSRCLN (NDB_No TEXT, Nutr_no TEXT, DataSrc_ID TEXT);
 CREATE TABLE FD_GROUP (FdGrp_CD TEXT, FdGrp_Desc TEXT);
-CREATE TABLE FLAV_DAT (NDB_No TEXT, Nutr_no TEXT, Nutrient_name TEXT, Flav_Val decimal, Se decimal, n INTEGER, Min decimal, CC TEXT, Max decimal);
-CREATE TABLE FLAV_IND (NDB_No TEXT, DataSrc_ID TEXT, Food_No TEXT, FoodIndiv_Desc TEXT, Method TEXT, Cmpd_Name TEXT, Rptd_CmpdVal decimal, Rptd_Std_Dev TEXT, Num_Data_Pts decimal, LT TEXT, Rptd_Units TEXT, Fresh_Dry_Wt TEXT, Quant_Std TEXT, Conv_Factor_G decimal, Conv_Factor_M TEXT, Conv_Factor_SpGr decimal, Cmpd_Val decimal, Cmpt_StdDev TEXT);
+CREATE TABLE FLAV_DAT (NDB_No TEXT, Nutr_no TEXT, Nutrient_name TEXT, Flav_Val DECIMAL, Se DECIMAL, n INTEGER, Min DECIMAL, CC TEXT, Max DECIMAL);
+CREATE TABLE FLAV_IND (NDB_No TEXT, DataSrc_ID TEXT, Food_No TEXT, FoodIndiv_Desc TEXT, Method TEXT, Cmpd_Name TEXT, Rptd_CmpdVal DECIMAL, Rptd_Std_Dev TEXT, Num_Data_Pts DECIMAL, LT TEXT, Rptd_Units TEXT, Fresh_Dry_Wt TEXT, Quant_Std TEXT, Conv_Factor_G DECIMAL, Conv_Factor_M TEXT, Conv_Factor_SpGr DECIMAL, Cmpd_Val DECIMAL, Cmpt_StdDev TEXT);
 CREATE TABLE FOOD_DES (NDB_No TEXT, FdGrp_Cd TEXT, Long_Desc TEXT, SciName TEXT);
 CREATE TABLE NUTR_DEF (Nutrr_no TEXT, Nutrient_name TEXT, Flav_Class TEXT, Unit TEXT, Tagname TEXT);
 CREATE UNIQUE INDEX DATA_SRC_PrimaryKey ON DATA_SRC (DataSrc_ID );
