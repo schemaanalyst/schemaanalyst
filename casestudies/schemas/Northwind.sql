b306eaeba760b7ccbe32f6ded50e76faee50638a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2013-08-17 16:35:48 -0400

    Minor modifications to the Northwind.sql schema.

--- casestudies/schemas/Northwind.sql
+++ casestudies/schemas/Northwind.sql
@@ -5,12 +5,12 @@
 
 -- https://northwindextended.googlecode.com/files/northwind.postgre.sql
 
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
+-- SET statement_timeout = 0;
+-- SET client_encoding = 'UTF8';
+-- SET standard_conforming_strings = on;
+-- SET check_function_bodies = false;
+-- SET client_min_messages = warning;
+-- 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
