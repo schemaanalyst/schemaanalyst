d88c9119e80cf1723d454c976b5ec99125ffc65f
Phil McMinn <p.mcminn@sheffield.ac.uk>
2013-08-19 17:04:58 +0100

    Adding in javadocs for Mutator.java

--- src/org/schemaanalyst/mutation/mutator/Mutator.java
+++ src/org/schemaanalyst/mutation/mutator/Mutator.java
@@ -8,66 +8,133 @@ import org.schemaanalyst.mutation.MutationPipeline;
 import org.schemaanalyst.mutation.supplier.Supplier;
 import org.schemaanalyst.util.Duplicable;
 
+/**
+ * A {@link Mutator} takes a {@link Supplier} to generate duplicates of an
+ * artefact and retrieve components from those duplications for mutation. In
+ * particular, the {@link #mutate()} method implements the protocol for handling
+ * a supplier. The class provides abstract methods which should be overridden in
+ * order to actually perform mutation on those components.
+ * 
+ * @author Phil McMinn
+ * 
+ * @param <A>
+ *            the type of the artefact being mutated.
+ * @param <C>
+ *            the type of component of the artefact which is to be mutated.
+ */
+
 public abstract class Mutator<A extends Duplicable<A>, C> extends
-        MutationPipeline<A> {
+		MutationPipeline<A> {
 
-    protected Supplier<A, C> supplier;
+	/**
+	 * The supplier for this mutator.
+	 */
+	protected Supplier<A, C> supplier;
 
-    public Mutator(Supplier<A, C> supplier) {
-        this.supplier = supplier;
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param supplier
+	 *            the supplier of duplicate artefacts and components for this
+	 *            mutator.
+	 */
+	public Mutator(Supplier<A, C> supplier) {
+		this.supplier = supplier;
+	}
 
-    public List<Mutant<A>> mutate() {
-        // Create the collection in which to store created mutants.
-        List<Mutant<A>> mutants = new ArrayList<>();
+	/**
+	 * Interacts with the supplier to mutate the components of its duplicates.
+	 */
+	public List<Mutant<A>> mutate() {
+		// Create the collection in which to store created mutants.
+		List<Mutant<A>> mutants = new ArrayList<>();
 
-        // are there more components to mutate?
-        while (supplier.hasNext()) {
+		// are there more components to mutate?
+		while (supplier.hasNext()) {
 
-            // get the reference component from the original artefact to mutate
-            C component = supplier.getNextComponent();
+			// get the reference component from the original artefact to mutate
+			C component = supplier.getNextComponent();
 
-            // perform any initialisation steps
-            initialise(component);
+			// perform any initialisation steps
+			initialise(component);
 
-            while (isMoreMutationToDo()) {
-                // make a duplicate of the original artefact
-                A duplicate = supplier.makeDuplicate();
+			while (isMoreMutationToDo()) {
+				// make a duplicate of the original artefact
+				A duplicate = supplier.makeDuplicate();
 
-                // get the component to be mutated from the duplicate
-                C componentToMutate = supplier.getDuplicateComponent();
+				// get the component to be mutated from the duplicate
+				C componentToMutate = supplier.getDuplicateComponent();
 
-                // do the mutation
-                C mutatedComponent = performMutation(componentToMutate);
+				// do the mutation
+				C mutatedComponent = performMutation(componentToMutate);
 
-                // put the mutated component back in the duplicate
-                supplier.putComponentBackInDuplicate(mutatedComponent);
+				// put the mutated component back in the duplicate
+				supplier.putComponentBackInDuplicate(mutatedComponent);
 
-                // make a mutant from the duplicate
-                Mutant<A> mutant = new Mutant<>(duplicate, 
-                		getOperatorInfo() + " - " + getDescriptionOfLastMutation());
+				// make a mutant from the duplicate
+				Mutant<A> mutant = new Mutant<>(duplicate, getOperatorInfo()
+						+ " - " + getDescriptionOfLastMutation());
 
-                // add it to the set of mutants
-                mutants.add(mutant);
-            }
-        }
+				// add it to the set of mutants
+				mutants.add(mutant);
+			}
+		}
 
-        return mutants;
-    }
+		return mutants;
+	}
 
-    protected abstract void initialise(C componentFromOriginalArtefact);
+	/**
+	 * Performs any initialisation steps before the mutation using the component
+	 * from the original artefact (for example creating instance variables to
+	 * cache collections that are to be iterated over in
+	 * {@link #performMutation(Object)}. This component should not be changed,
+	 * only its duplicates in the {@link #performMutation} method.
+	 * 
+	 * @param componentFromOriginalArtefact
+	 *            the component from the original artefact.
+	 */
+	protected abstract void initialise(C componentFromOriginalArtefact);
 
-    protected abstract boolean isMoreMutationToDo();
+	/**
+	 * Returns true when all mutation has been exhausted for the current
+	 * component (that is, the object passed to {@link #initialise(Object)},
+	 * signalling that the next one should be obtained from the supplier.
+	 * 
+	 * @return True if all mutation has been performed on the current component.
+	 */
+	protected abstract boolean isMoreMutationToDo();
 
-    protected abstract C performMutation(C componentToMutate);
-    
-    protected abstract String getDescriptionOfLastMutation();
-    
-    public String getOperatorInfo() {
-    	return getClass().getSimpleName() + " with " + supplier;
-    }
-    
-    public String toString() {
-        return getOperatorInfo();
-    }
+	/**
+	 * Performs mutation on a copy of the object originallly passed to
+	 * {@link #initialise(Object)}.
+	 * 
+	 * @param componentToMutate
+	 *            the component to mutate.
+	 * @return the mutated component.
+	 */
+	protected abstract C performMutation(C componentToMutate);
+
+	/**
+	 * Returns a description of what occurred in the last call to
+	 * {@link #performMutation(Object)} used to accompany the generated mutants
+	 * returned from {@link #mutate()}.
+	 * 
+	 * @return a descriptor string of the last mutation operation.
+	 */
+	protected abstract String getDescriptionOfLastMutation();
+
+	/**
+	 * Returns an information detailing the current mutator and the supplier
+	 * being used.
+	 * 
+	 * @return a descriptor string.
+	 */
+	public String getOperatorInfo() {
+		return getClass().getSimpleName() + " with " + supplier;
+	}
+
+	@Override
+	public String toString() {
+		return getOperatorInfo();
+	}
 }
