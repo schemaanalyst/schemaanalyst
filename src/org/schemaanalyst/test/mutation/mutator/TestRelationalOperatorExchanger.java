335e2bd7c8ea9cebf1bf4341b367da57e7e01bb1
Chris Wright <aca08cw@sheffield.ac.uk>
2013-08-21 16:08:13 +0100

    Improving test self-documentation

--- src/org/schemaanalyst/test/mutation/mutator/TestRelationalOperatorExchanger.java
+++ src/org/schemaanalyst/test/mutation/mutator/TestRelationalOperatorExchanger.java
@@ -16,26 +16,24 @@ import org.schemaanalyst.sqlrepresentation.expression.ConstantExpression;
 import org.schemaanalyst.sqlrepresentation.expression.Expression;
 import org.schemaanalyst.sqlrepresentation.expression.RelationalExpression;
 
-/**
- *
- * @author Chris J. Wright
- */
 public class TestRelationalOperatorExchanger {
-    
+
     RelationalExpression relationalExpression1 = new RelationalExpression(
             new ConstantExpression(new NumericValue(1)),
             RelationalOperator.LESS,
             new ConstantExpression(new NumericValue(2)));
-    
     RelationalExpression relationalExpression2 = new RelationalExpression(
             new ConstantExpression(new NumericValue(1)),
             RelationalOperator.EQUALS,
             new ConstantExpression(new NumericValue(2)));
-    
+
+    /**
+     * Mock implementation of a Supplier
+     */
     class MockSupplier extends Supplier<Expression, RelationalOperator> {
 
         boolean hasNext = true, haveCurrent = false;
-        
+
         @Override
         public boolean hasNext() {
             return hasNext;
@@ -55,18 +53,20 @@ public class TestRelationalOperatorExchanger {
 
         @Override
         public RelationalOperator getDuplicateComponent() {
-            return ((RelationalExpression)currentDuplicate).getRelationalOperator();
+            return ((RelationalExpression) currentDuplicate).getRelationalOperator();
         }
 
         @Override
         public void putComponentBackInDuplicate(RelationalOperator component) {
             ((RelationalExpression) currentDuplicate).setRelationalOperator(component);
         }
-        
     }
-    
+
+    /**
+     * Tests the exchanging of a single &lt; operator in a relational expression.
+     */
     @Test
-    public void testExchange() {
+    public void testExchangeLess() {
         MockSupplier supplier = new MockSupplier();
         supplier.initialise(relationalExpression1);
         RelationalOperatorExchanger exchanger = new RelationalOperatorExchanger(supplier);
@@ -75,13 +75,19 @@ public class TestRelationalOperatorExchanger {
         Set<RelationalOperator> ops = EnumSet.noneOf(RelationalOperator.class);
         for (Mutant<Expression> mutant : mutants) {
             RelationalOperator op = ((RelationalExpression) mutant.getMutatedArtefact()).getRelationalOperator();
-            assertFalse(op.equals(RelationalOperator.LESS));
-            assertFalse(ops.contains(op));
+            assertFalse("The original relational operator should not occur in any mutant",
+                    op.equals(RelationalOperator.LESS));
+            assertFalse("The same relational operator should not occur in two mutants",
+                    ops.contains(op));
             ops.add(op);
         }
-        assertEquals(5, ops.size());
+        assertEquals("There should be exactly 5 mutants produced when replacing one relational operator",
+                5, ops.size());
     }
-    
+
+    /**
+     * Tests the exchanging of a single = operator in a relational expression.
+     */
     @Test
     public void testExchangeEquals() {
         MockSupplier supplier = new MockSupplier();
@@ -92,11 +98,13 @@ public class TestRelationalOperatorExchanger {
         Set<RelationalOperator> ops = EnumSet.noneOf(RelationalOperator.class);
         for (Mutant<Expression> mutant : mutants) {
             RelationalOperator op = ((RelationalExpression) mutant.getMutatedArtefact()).getRelationalOperator();
-            assertFalse(op.equals(RelationalOperator.EQUALS));
-            assertFalse(ops.contains(op));
+            assertFalse("The original relational operator should not occur in any mutant",
+                    op.equals(RelationalOperator.EQUALS));
+            assertFalse("The same relational operator should not occur in two mutants",
+                    ops.contains(op));
             ops.add(op);
         }
-        assertEquals(5, ops.size());
+        assertEquals("There should be exactly 5 mutants produced when replacing one relational operator",
+                5, ops.size());
     }
-    
 }
