8f73310234e3a08ea15fd91f5efd425000a01b69
Chris Wright <aca08cw@sheffield.ac.uk>
2013-08-23 10:37:44 +0100

    Updating TestCCRelationlExpressionOperatorE to a proper test set

--- src/org/schemaanalyst/test/mutation/operator/TestCCRelationalExpressionOperatorE.java
+++ src/org/schemaanalyst/test/mutation/operator/TestCCRelationalExpressionOperatorE.java
@@ -14,9 +14,12 @@ import org.schemaanalyst.sqlrepresentation.Schema;
 import org.schemaanalyst.sqlrepresentation.Table;
 import org.schemaanalyst.sqlrepresentation.constraint.CheckConstraint;
 import org.schemaanalyst.sqlrepresentation.datatype.IntDataType;
+import org.schemaanalyst.sqlrepresentation.expression.AndExpression;
 import org.schemaanalyst.sqlrepresentation.expression.ColumnExpression;
 import org.schemaanalyst.sqlrepresentation.expression.ConstantExpression;
 import org.schemaanalyst.sqlrepresentation.expression.Expression;
+import org.schemaanalyst.sqlrepresentation.expression.InExpression;
+import org.schemaanalyst.sqlrepresentation.expression.ListExpression;
 import org.schemaanalyst.sqlrepresentation.expression.RelationalExpression;
 
 /**
@@ -25,12 +28,32 @@ import org.schemaanalyst.sqlrepresentation.expression.RelationalExpression;
  */
 public class TestCCRelationalExpressionOperatorE {
 
-    Schema schema1 = new TestSchema1("schema");
-    List<Mutant<Schema>> schema1Mutants = new CCRelationalExpressionOperatorE(schema1).mutate();
+    private class TestSchemaNoRelational extends Schema {
 
-    private class TestSchema1 extends Schema {
+        public TestSchemaNoRelational(String name) {
+            super(name);
+            Table t1 = new Table("t1");
+            Column c1 = new Column("c1", new IntDataType());
+            t1.addColumn(c1);
+            this.addTable(t1);
+            Expression expr = new InExpression(
+                    new ColumnExpression(t1, c1),
+                    new ListExpression(new ConstantExpression(new NumericValue(1)), new ConstantExpression(new NumericValue(2))), false);
+            this.addCheckConstraint(new CheckConstraint(t1, expr));
+        }
+    }
+    Schema schemaNoRelational = new TestSchemaNoRelational("schema");
+    List<Mutant<Schema>> schemaNoRelationalMutants = new CCRelationalExpressionOperatorE(schemaNoRelational).mutate();
 
-        public TestSchema1(String name) {
+    @Test
+    public void testSchemaNoRelationalMutantNumber() {
+        assertEquals("Mutating a schema with no constraints with relational "
+                + "expressions should produce 0 mutants", 0, schemaNoRelationalMutants.size());
+    }
+
+    private class TestSchemaOneRelational extends Schema {
+
+        public TestSchemaOneRelational(String name) {
             super(name);
             Table t1 = new Table("t1");
             Column c1 = new Column("c1", new IntDataType());
@@ -43,39 +66,153 @@ public class TestCCRelationalExpressionOperatorE {
             this.addCheckConstraint(new CheckConstraint(t1, expr));
         }
     }
+    Schema schemaOneRelational = new TestSchemaOneRelational("schema");
+    List<Mutant<Schema>> schemaOneRelationalMutants = new CCRelationalExpressionOperatorE(schemaOneRelational).mutate();
 
     @Test
-    public void testSchema1MutantNumber() {
-        assertEquals("Three mutants should be produced", 5, schema1Mutants.size());
+    public void testSchemaOneRelationalMutantNumber() {
+        assertEquals("Mutating a schema with one constraint with one relational"
+                + " expression should produce 5 mutants", 5, schemaOneRelationalMutants.size());
+    }
+
+    public void testSchemaOneRelationalMutant(int mutantNumber, RelationalOperator op) {
+        Schema mutant = schemaOneRelationalMutants.get(mutantNumber).getMutatedArtefact();
+        Table t1 = mutant.getTable("t1");
+        Column c1 = t1.getColumn("c1");
+        Expression expr = new RelationalExpression(
+                new ColumnExpression(t1, c1),
+                op,
+                new ConstantExpression(new NumericValue(5)));
+        assertEquals("Mutating a schema with one check constraint should produce"
+                + "a mutant with one check constraint",
+                1, mutant.getCheckConstraints().size());
+        assertEquals("The mutated expression in mutant " + mutantNumber
+                + " should replace < with " + op,
+                expr, mutant.getCheckConstraints().get(0).getExpression());
     }
 
     @Test
-    public void testSchema1Mutant1() {
-        assertNotEquals("Mutation has not been applied in duplicate", "c1 < 5",
-                schema1Mutants.get(0).getMutatedArtefact().getCheckConstraints().get(0).getExpression().toString());
+    public void testSchemaOneRelationalMutant1() {
+        testSchemaOneRelationalMutant(0, RelationalOperator.EQUALS);
     }
 
     @Test
-    public void testSchema1Mutant2() {
-        assertNotEquals("Mutation has not been applied in duplicate", "c1 < 5",
-                schema1Mutants.get(1).getMutatedArtefact().getCheckConstraints().get(0).getExpression().toString());
+    public void testSchemaOneRelationalMutant2() {
+        testSchemaOneRelationalMutant(1, RelationalOperator.NOT_EQUALS);
     }
 
     @Test
-    public void testSchema1Mutant3() {
-        assertNotEquals("Mutation has not been applied in duplicate", "c1 < 5",
-                schema1Mutants.get(2).getMutatedArtefact().getCheckConstraints().get(0).getExpression().toString());
+    public void testSchemaOneRelationalMutant3() {
+        testSchemaOneRelationalMutant(2, RelationalOperator.GREATER);
+    }
+
+    @Test
+    public void testSchemaOneRelationalMutant4() {
+        testSchemaOneRelationalMutant(3, RelationalOperator.GREATER_OR_EQUALS);
+    }
+
+    @Test
+    public void testSchemaOneRelationalMutant5() {
+        testSchemaOneRelationalMutant(4, RelationalOperator.LESS_OR_EQUALS);
+    }
+
+    private class TestSchemaTwoRelational extends Schema {
+
+        public TestSchemaTwoRelational(String name) {
+            super(name);
+            Table t1 = new Table("t1");
+            Column c1 = new Column("c1", new IntDataType());
+            t1.addColumn(c1);
+            this.addTable(t1);
+            Expression expr = new AndExpression(
+                    new RelationalExpression(
+                    new ColumnExpression(t1, c1),
+                    RelationalOperator.LESS,
+                    new ConstantExpression(new NumericValue(5))),
+                    new RelationalExpression(
+                    new ColumnExpression(t1, c1),
+                    RelationalOperator.GREATER_OR_EQUALS,
+                    new ConstantExpression(new NumericValue(0))));
+            this.addCheckConstraint(new CheckConstraint(t1, expr));
+        }
+    }
+    Schema schemaTwoRelational = new TestSchemaTwoRelational("schema");
+    List<Mutant<Schema>> schemaTwoRelationalMutants = new CCRelationalExpressionOperatorE(schemaTwoRelational).mutate();
+
+    @Test
+    public void testSchemaTwoRelationalMutantNumber() {
+        assertEquals("Mutating a schema with one constraint with two relational"
+                + " expressions should produce 10 mutants", 10, schemaTwoRelationalMutants.size());
+    }
+
+    public void testSchemaTwoRelationalMutant(int mutantNumber, RelationalOperator op1, RelationalOperator op2) {
+        Schema mutant = schemaTwoRelationalMutants.get(mutantNumber).getMutatedArtefact();
+        Table t1 = mutant.getTable("t1");
+        Column c1 = t1.getColumn("c1");
+        Expression expr = new AndExpression(
+                    new RelationalExpression(
+                    new ColumnExpression(t1, c1),
+                    op1,
+                    new ConstantExpression(new NumericValue(5))),
+                    new RelationalExpression(
+                    new ColumnExpression(t1, c1),
+                    op2,
+                    new ConstantExpression(new NumericValue(0))));
+        assertEquals("Mutating a schema with one check constraint should produce"
+                + "a mutant with one check constraint",
+                1, mutant.getCheckConstraints().size());
+        assertEquals("The mutated expression in mutant " + mutantNumber + " should "
+                + "have a first operator of " + op1 + " and a second operator of " + op2,
+                expr, mutant.getCheckConstraints().get(0).getExpression());
     }
     
     @Test
-    public void testSchema1Mutant4() {
-        assertNotEquals("Mutation has not been applied in duplicate", "c1 < 5",
-                schema1Mutants.get(3).getMutatedArtefact().getCheckConstraints().get(0).getExpression().toString());
+    public void testSchemaTwoRelationalMutant1() {
+        testSchemaTwoRelationalMutant(0, RelationalOperator.EQUALS, RelationalOperator.GREATER_OR_EQUALS);
     }
     
     @Test
-    public void testSchema1Mutant5() {
-        assertNotEquals("Mutation has not been applied in duplicate", "c1 < 5",
-                schema1Mutants.get(4).getMutatedArtefact().getCheckConstraints().get(0).getExpression().toString());
+    public void testSchemaTwoRelationalMutant2() {
+        testSchemaTwoRelationalMutant(1, RelationalOperator.NOT_EQUALS, RelationalOperator.GREATER_OR_EQUALS);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant3() {
+        testSchemaTwoRelationalMutant(2, RelationalOperator.GREATER, RelationalOperator.GREATER_OR_EQUALS);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant4() {
+        testSchemaTwoRelationalMutant(3, RelationalOperator.GREATER_OR_EQUALS, RelationalOperator.GREATER_OR_EQUALS);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant5() {
+        testSchemaTwoRelationalMutant(4, RelationalOperator.LESS_OR_EQUALS, RelationalOperator.GREATER_OR_EQUALS);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant6() {
+        testSchemaTwoRelationalMutant(5, RelationalOperator.LESS, RelationalOperator.EQUALS);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant7() {
+        testSchemaTwoRelationalMutant(6, RelationalOperator.LESS, RelationalOperator.NOT_EQUALS);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant8() {
+        testSchemaTwoRelationalMutant(7, RelationalOperator.LESS, RelationalOperator.GREATER);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant9() {
+        testSchemaTwoRelationalMutant(8, RelationalOperator.LESS, RelationalOperator.LESS);
+    }
+    
+    @Test
+    public void testSchemaTwoRelationalMutant10() {
+        testSchemaTwoRelationalMutant(9, RelationalOperator.LESS, RelationalOperator.LESS_OR_EQUALS);
     }
 }
