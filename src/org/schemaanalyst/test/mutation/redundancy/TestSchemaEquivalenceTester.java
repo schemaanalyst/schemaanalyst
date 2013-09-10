01fb051a4dba3a5a766150ef9f58ab7dcfe894b9
Chris Wright <aca08cw@sheffield.ac.uk>
2013-09-10 10:39:26 +0100

    Adding test for constructor with supplied arguments, to get 100 percent coverage

--- src/org/schemaanalyst/test/mutation/redundancy/TestSchemaEquivalenceTester.java
+++ src/org/schemaanalyst/test/mutation/redundancy/TestSchemaEquivalenceTester.java
@@ -4,21 +4,49 @@ package org.schemaanalyst.test.mutation.redundancy;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.schemaanalyst.data.NumericValue;
+import org.schemaanalyst.logic.RelationalOperator;
+import org.schemaanalyst.mutation.redundancy.CheckEquivalenceTester;
+import org.schemaanalyst.mutation.redundancy.ColumnEquivalenceTester;
+import org.schemaanalyst.mutation.redundancy.ForeignKeyEquivalenceTester;
+import org.schemaanalyst.mutation.redundancy.PrimaryKeyEquivalenceTester;
 import org.schemaanalyst.mutation.redundancy.SchemaEquivalenceTester;
+import org.schemaanalyst.mutation.redundancy.TableEquivalenceTester;
+import org.schemaanalyst.mutation.redundancy.UniqueEquivalenceTester;
 import org.schemaanalyst.sqlrepresentation.*;
+import org.schemaanalyst.sqlrepresentation.constraint.CheckConstraint;
 import org.schemaanalyst.sqlrepresentation.constraint.ForeignKeyConstraint;
 import org.schemaanalyst.sqlrepresentation.constraint.PrimaryKeyConstraint;
 import org.schemaanalyst.sqlrepresentation.constraint.UniqueConstraint;
 import org.schemaanalyst.sqlrepresentation.datatype.*;
+import org.schemaanalyst.sqlrepresentation.expression.ColumnExpression;
+import org.schemaanalyst.sqlrepresentation.expression.ConstantExpression;
+import org.schemaanalyst.sqlrepresentation.expression.RelationalExpression;
 
 /**
  *
  * @author Chris J. Wright
  */
 public class TestSchemaEquivalenceTester {
-
-    {
-        assertTrue(true);
+    
+    @Test
+    public void testConstructor() {
+        SchemaEquivalenceTester tester1 = new SchemaEquivalenceTester();
+        SchemaEquivalenceTester tester2 = new SchemaEquivalenceTester(
+                new TableEquivalenceTester(new ColumnEquivalenceTester()),
+                new ColumnEquivalenceTester(),
+                new PrimaryKeyEquivalenceTester(),
+                new ForeignKeyEquivalenceTester(),
+                new UniqueEquivalenceTester(),
+                new CheckEquivalenceTester());
+        Schema s = new Schema("s");
+        Table t = s.createTable("t");
+        Column a = t.createColumn("a", new IntDataType());
+        assertEquals("A SchemaEquivalenceTester should give the same results, "
+                + "when constructed by the default constructor and by the "
+                + "parameterised constructor (given the same sub-component "
+                + "EquivalenceTester classes as the defaults)",
+                tester1.areEquivalent(s, s), tester2.areEquivalent(s, s));
     }
 
     @Test
@@ -42,6 +70,22 @@ public class TestSchemaEquivalenceTester {
         Column a2 = t2.createColumn("a", new IntDataType());
         assertTrue("Two schemas with the same tables and columns should be "
                 + "equivalent", tester.areEquivalent(s1, s2));
+    }
+    
+    @Test
+    public void testDifferentIdentifiers() {
+        SchemaEquivalenceTester tester = new SchemaEquivalenceTester();
+        Schema s1 = new Schema("schemaOne");
+        Table t1 = s1.createTable("t");
+        Column a1 = t1.createColumn("a", new IntDataType());
+        Schema s2 = new Schema("schemaTwo");
+        Table t2 = s2.createTable("t");
+        Column a2 = t2.createColumn("a", new IntDataType());
+        assertFalse("Two schemas with different identifiers should not be "
+                + "equivalent", tester.areEquivalent(s1, s2));
+        s2.setName("schemaOne");
+        assertTrue("Changing the identifier of a schema should be able to make "
+                + "two otherwise identical schemas equivalent", tester.areEquivalent(s1, s2));
     }
 
     @Test
@@ -104,7 +148,7 @@ public class TestSchemaEquivalenceTester {
         assertTrue("The ordering of tables in a schema should not affect the "
                 + "detection of equivalence", tester.areEquivalent(s1, s2));
     }
-    
+
     @Test
     public void testMissingPrimaryKey() {
         SchemaEquivalenceTester tester = new SchemaEquivalenceTester();
@@ -121,7 +165,7 @@ public class TestSchemaEquivalenceTester {
         assertTrue("Adding a primary key should be able to make two schemas "
                 + "equivalent", tester.areEquivalent(s1, s2));
     }
-    
+
     @Test
     public void testMultiplePrimaryKeys() {
         SchemaEquivalenceTester tester = new SchemaEquivalenceTester();
@@ -204,7 +248,7 @@ public class TestSchemaEquivalenceTester {
         assertTrue("Adding a foreign key constraint should be able to make two "
                 + "otherwise identical schemas equivalent", tester.areEquivalent(s1, s2));
     }
-    
+
     @Test
     public void testMultipleForeignKeys() {
         SchemaEquivalenceTester tester = new SchemaEquivalenceTester();
@@ -229,6 +273,60 @@ public class TestSchemaEquivalenceTester {
                 + " a schema with one foreign key", tester.areEquivalent(s1, s2));
         s2.addForeignKeyConstraint(new ForeignKeyConstraint(t2, c2, u2, d2));
         assertTrue("Adding a foreign key constraint should be able to make two "
+                + "otherwise identical schemas equivalent", tester.areEquivalent(s1, s2));
+    }
+
+    @Test
+    public void testMissingCheck() {
+        SchemaEquivalenceTester tester = new SchemaEquivalenceTester();
+        Schema s1 = new Schema("s");
+        Table t1 = s1.createTable("t");
+        Column a1 = t1.createColumn("a", new IntDataType());
+        s1.addCheckConstraint(new CheckConstraint(t1, new RelationalExpression(
+                new ColumnExpression(t1, a1),
+                RelationalOperator.LESS,
+                new ConstantExpression(new NumericValue(5)))));
+        Schema s2 = new Schema("s");
+        Table t2 = s2.createTable("t");
+        Column a2 = t2.createColumn("a", new IntDataType());
+        assertFalse("A schema with a check constraint should not be equivalent "
+                + "to a schema without a check constraint", tester.areEquivalent(s1, s2));
+        s2.addCheckConstraint(new CheckConstraint(t2, new RelationalExpression(
+                new ColumnExpression(t2, a2),
+                RelationalOperator.LESS,
+                new ConstantExpression(new NumericValue(5)))));
+        assertTrue("Adding a check constraint should be able to make two "
+                + "otherwise identical schemas equivalent", tester.areEquivalent(s1, s2));
+    }
+    
+    @Test
+    public void testMultipleChecks() {
+        SchemaEquivalenceTester tester = new SchemaEquivalenceTester();
+        Schema s1 = new Schema("s");
+        Table t1 = s1.createTable("t");
+        Column a1 = t1.createColumn("a", new IntDataType());
+        s1.addCheckConstraint(new CheckConstraint(t1, new RelationalExpression(
+                new ColumnExpression(t1, a1),
+                RelationalOperator.LESS,
+                new ConstantExpression(new NumericValue(5)))));
+        s1.addCheckConstraint(new CheckConstraint(t1, new RelationalExpression(
+                new ColumnExpression(t1, a1),
+                RelationalOperator.GREATER_OR_EQUALS,
+                new ConstantExpression(new NumericValue(0)))));
+        Schema s2 = new Schema("s");
+        Table t2 = s2.createTable("t");
+        Column a2 = t2.createColumn("a", new IntDataType());
+        s2.addCheckConstraint(new CheckConstraint(t2, new RelationalExpression(
+                new ColumnExpression(t2, a2),
+                RelationalOperator.LESS,
+                new ConstantExpression(new NumericValue(5)))));
+        assertFalse("A schema with two check constraints should not be "
+                + "equivalent to a schema without a check constraint", tester.areEquivalent(s1, s2));
+        s2.addCheckConstraint(new CheckConstraint(t2, new RelationalExpression(
+                new ColumnExpression(t2, a2),
+                RelationalOperator.GREATER_OR_EQUALS,
+                new ConstantExpression(new NumericValue(0)))));
+        assertTrue("Adding a check constraint should be able to make two "
                 + "otherwise identical schemas equivalent", tester.areEquivalent(s1, s2));
     }
 }
