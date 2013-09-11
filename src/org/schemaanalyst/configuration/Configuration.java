0794f0680a8b504861cedfd4931aeb707d1ebb29
Chris Wright <aca08cw@sheffield.ac.uk>
2013-09-11 17:55:23 +0100

    configuration package: javadoc

--- src/org/schemaanalyst/configuration/Configuration.java
+++ src/org/schemaanalyst/configuration/Configuration.java
@@ -10,6 +10,27 @@ import org.schemaanalyst.util.PropertiesParser;
  * @author Chris J. Wright
  */
 class Configuration {
+    /**
+     * <p>
+     * Loads the contents of a configuration file into a given instance.
+     * </p>
+     * 
+     * <p>
+     * <i>
+     * <b>
+     * Note:
+     * </b>
+     * To allow for non-version controlled local copies of 
+     * configuration files, if there exists a file at {@code location + 
+     * ".local"} this will be loaded instead of the file at {@code location}.
+     * </i>
+     * </p>
+     * 
+     * @param <T> The {@link Configuration} type
+     * @param location The relative path to the file
+     * @param t The {@link Configuration} instance
+     * @return The {@link Configuration} instance with content loaded
+     */
     protected <T> T load(String location, T t) {
         if (new File(location + ".local").exists()) {
             location += ".local";
