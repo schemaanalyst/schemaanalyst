adf9aa9a3f4e9b0d367a524007825221570db560
Chris Wright <aca08cw@sheffield.ac.uk>
2013-09-13 10:11:21 +0100

    New pipeline with improved schema equivalence checker

---
+++ src/org/schemaanalyst/mutation/pipeline/ICST2013NewSchemaPipeline.java
@@ -0,0 +1,39 @@
+/*
+ */
+
+package org.schemaanalyst.mutation.pipeline;
+
+import org.schemaanalyst.mutation.equivalence.SchemaEquivalanceWithNotNullCheckChecker;
+import org.schemaanalyst.mutation.operator.CCNullifier;
+import org.schemaanalyst.mutation.operator.FKCColumnPairR;
+import org.schemaanalyst.mutation.operator.NNCAR;
+import org.schemaanalyst.mutation.operator.PKCColumnARE;
+import org.schemaanalyst.mutation.operator.UCColumnARE;
+import org.schemaanalyst.mutation.redundancy.MutantEquivalentToMutantRemover;
+import org.schemaanalyst.mutation.redundancy.MutantEquivalentToOriginalRemover;
+import org.schemaanalyst.mutation.redundancy.PrimaryKeyColumnNotNullRemover;
+import org.schemaanalyst.mutation.redundancy.PrimaryKeyColumnsUniqueRemover;
+import org.schemaanalyst.sqlrepresentation.Schema;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Chris J. Wright
+ */
+public class ICST2013NewSchemaPipeline extends MutationPipeline<Schema> {
+
+    public ICST2013NewSchemaPipeline(Schema schema) {
+        addProducer(new CCNullifier(schema));
+        addProducer(new FKCColumnPairR(schema));
+        addProducer(new PKCColumnARE(schema));
+        addProducer(new NNCAR(schema));
+        addProducer(new UCColumnARE(schema));
+
+        addRemover(new PrimaryKeyColumnNotNullRemover());
+        addRemover(new PrimaryKeyColumnsUniqueRemover());
+        addRemover(new MutantEquivalentToOriginalRemover<>(new SchemaEquivalanceWithNotNullCheckChecker(), schema));
+        addRemover(new MutantEquivalentToMutantRemover<>(new SchemaEquivalanceWithNotNullCheckChecker()));
+    }
+}
