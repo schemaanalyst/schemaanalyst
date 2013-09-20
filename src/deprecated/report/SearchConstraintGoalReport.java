ab8cf6f5ce4c75014e20ea61480e3690d2c5b989
Phil McMinn <p.mcminn@sheffield.ac.uk>
2013-09-20 10:08:58 +0100

    adding missing deprecated files

---
+++ src/deprecated/report/SearchConstraintGoalReport.java
@@ -0,0 +1,59 @@
+package deprecated.report;
+
+import org.schemaanalyst.datageneration.search.objective.ObjectiveValue;
+import org.schemaanalyst.sqlrepresentation.constraint.Constraint;
+
+public class SearchConstraintGoalReport extends ConstraintGoalReport {
+
+    protected int numEvaluations, numRestarts;
+    protected ObjectiveValue bestObjectiveValue;
+
+    public SearchConstraintGoalReport(Constraint constraint) {
+        super(constraint);
+        numEvaluations = 0;
+        numRestarts = 0;
+    }
+
+    public int getNumEvaluations() {
+        return numEvaluations;
+    }
+
+    public void setNumEvaluations(int numEvaluations) {
+        this.numEvaluations = numEvaluations;
+    }
+
+    public int getNumRestarts() {
+        return numRestarts;
+    }
+
+    public void setNumRestarts(int numRestarts) {
+        this.numRestarts = numRestarts;
+    }
+
+    public void setBestObjectiveValue(ObjectiveValue bestObjectiveValue) {
+        this.bestObjectiveValue = bestObjectiveValue;
+    }
+
+    public ObjectiveValue getBestObjectiveValue() {
+        return bestObjectiveValue;
+    }
+
+    @Override
+    public void appendToStringBuilder(StringBuilder sb) {
+        super.appendToStringBuilder(sb);
+
+        sb.append("-- * Number of objective function evaluations: ");
+        sb.append(getNumEvaluations());
+        sb.append("\n");
+
+        sb.append("-- * Number of restarts: ");
+        sb.append(getNumRestarts());
+        sb.append("\n");
+
+        if (!wasSuccess()) {
+            sb.append("/* Objective value:\n");
+            getBestObjectiveValue().appendToStringBuilder(sb, " ");
+            sb.append("*/ \n");
+        }
+    }
+}
