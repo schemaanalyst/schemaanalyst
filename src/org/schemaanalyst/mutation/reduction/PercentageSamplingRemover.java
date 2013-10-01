4038e7553348ad46d2df92f5ff3de2111c0cdaa4
Chris Wright <aca08cw@sheffield.ac.uk>
2013-10-01 14:26:03 +0100

    Adding sampling remover using percentage

---
+++ src/org/schemaanalyst/mutation/reduction/PercentageSamplingRemover.java
@@ -0,0 +1,56 @@
+/*
+ */
+package org.schemaanalyst.mutation.reduction;
+
+import java.util.List;
+import java.util.Random;
+import org.schemaanalyst.mutation.Mutant;
+import org.schemaanalyst.mutation.pipeline.MutantRemover;
+
+/**
+ * <p>
+ * A {@link MutantRemover} takes a list of mutants and randomly selects a given 
+ * percentage of mutants to retain.
+ * </p>
+ * 
+ * <p>
+ * Note that for very large lists of mutants this may give poor performance, as 
+ * the list must be shuffled.
+ * </p>
+ *
+ * @author Chris J. Wright
+ */
+public class PercentageSamplingRemover<T> extends MutantRemover<T> {
+
+    private final double percent;
+    private final Random random;
+
+    /**
+     * Constructor with a specified random number generator, used to randomly 
+     * select the elements.
+     * 
+     * @param percent
+     * @param random 
+     */
+    public PercentageSamplingRemover(double percent, Random random) {
+        this.percent = percent;
+        this.random = random;
+    }
+
+    /**
+     * Constructor with a default random number generator, used to randomly 
+     * select the elements.
+     * 
+     * @param count 
+     */
+    public PercentageSamplingRemover(double percent) {
+        this.percent = percent;
+        this.random = new Random();
+    }
+
+    @Override
+    public List<Mutant<T>> removeMutants(List<Mutant<T>> mutants) {
+        int count = (int) Math.round(percent * mutants.size());
+        return new SamplingRemover<T>(count, random).removeMutants(mutants);
+    }
+}
