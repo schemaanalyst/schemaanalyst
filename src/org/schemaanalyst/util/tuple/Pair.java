55acebfd6c5488de778ed028f53a497b3fd1191c
Chris Wright <aca08cw@sheffield.ac.uk>
2013-10-03 11:41:57 +0100

    Adding toString for pairs

--- src/org/schemaanalyst/util/tuple/Pair.java
+++ src/org/schemaanalyst/util/tuple/Pair.java
@@ -5,4 +5,9 @@ public class Pair<T> extends MixedPair<T, T> {
     public Pair(T first, T second) {
         super(first, second);
     }  
+
+    @Override
+    public String toString() {
+        return "Pair(" + first + ", " + second + ")";
+    }
 }
