9e204ef8a223e5d68e78f166807da85a7f5ffa1f
Chris Wright <aca08cw@sheffield.ac.uk>
2013-11-15 16:26:46 -0500

    Breaking out NNCAR into A and R components

---
+++ src/org/schemaanalyst/mutation/operator/NNCA.java
@@ -0,0 +1,57 @@
+package org.schemaanalyst.mutation.operator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.schemaanalyst.mutation.Mutant;
+import org.schemaanalyst.mutation.MutantProducer;
+import org.schemaanalyst.sqlrepresentation.Column;
+import org.schemaanalyst.sqlrepresentation.Schema;
+import org.schemaanalyst.sqlrepresentation.Table;
+import org.schemaanalyst.sqlrepresentation.constraint.NotNullConstraint;
+
+/**
+ * <p>
+ * A {@link MutantProducer} that mutates {@link Schema}s by adding a
+ * {@link NotNullConstraint} to each column in turn.
+ * </p>
+ *
+ * @author Chris J. Wright
+ *
+ */
+public class NNCA implements MutantProducer<Schema> {
+
+    private Schema schema;
+
+    public NNCA(Schema schema) {
+        this.schema = schema;
+    }
+
+    @Override
+    public List<Mutant<Schema>> mutate() {
+        List<Mutant<Schema>> mutants = new ArrayList<>();
+
+        // Generate the mutants
+        for (Table table : schema.getTables()) {
+            for (Column column : table.getColumns()) {
+                if (!schema.isNotNull(table, column)) {
+                    // create a NOT NULL constraint on the column
+                    Schema dupAddSchema = schema.duplicate();
+                    Table dupAddTable = dupAddSchema.getTable(table.getName());
+                    Column dupAddColumn = dupAddTable.getColumn(column.getName());
+                    dupAddSchema.createNotNullConstraint(dupAddTable, dupAddColumn);
+                    mutants.add(new Mutant<>(dupAddSchema,
+                            "Added NOT NULL to column " + dupAddColumn
+                            + " in table " + dupAddTable));
+                }
+            }
+        }
+
+        // Set the producer information in each mutant
+        for (Mutant<Schema> mutant : mutants) {
+            mutant.setMutantProducer(this);
+        }
+
+        return mutants;
+    }
+}
---
+++ src/org/schemaanalyst/mutation/operator/NNCR.java
@@ -0,0 +1,60 @@
+package org.schemaanalyst.mutation.operator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.schemaanalyst.mutation.Mutant;
+import org.schemaanalyst.mutation.MutantProducer;
+import org.schemaanalyst.sqlrepresentation.Column;
+import org.schemaanalyst.sqlrepresentation.Schema;
+import org.schemaanalyst.sqlrepresentation.Table;
+import org.schemaanalyst.sqlrepresentation.constraint.NotNullConstraint;
+
+/**
+ * <p>
+ * A {@link MutantProducer} that mutates {@link Schema}s by removing a
+ * {@link NotNullConstraint} to each column in turn.
+ * </p>
+ *
+ * @author Chris J. Wright
+ *
+ */
+public class NNCR implements MutantProducer<Schema> {
+
+    private Schema schema;
+
+    public NNCR(Schema schema) {
+        this.schema = schema;
+    }
+
+    @Override
+    public List<Mutant<Schema>> mutate() {
+        List<Mutant<Schema>> mutants = new ArrayList<>();
+
+        // Generate the mutants
+        for (Table table : schema.getTables()) {
+            for (Column column : table.getColumns()) {
+                if (schema.isNotNull(table, column)) {
+                    // remove a NOT NULL constraint on the column
+                    Schema dupRemoveSchema = schema.duplicate();
+                    Table dupRemoveTable = dupRemoveSchema
+                            .getTable(table.getName());
+                    Column dupRemoveColumn = dupRemoveTable.getColumn(column
+                            .getName());
+                    dupRemoveSchema.removeNotNullConstraint(new NotNullConstraint(
+                            dupRemoveTable, dupRemoveColumn));
+                    mutants.add(new Mutant<>(dupRemoveSchema,
+                            "Removed NOT NULL to column " + dupRemoveColumn
+                                    + " in table " + dupRemoveTable));
+                }
+            }
+        }
+
+        // Set the producer information in each mutant
+        for (Mutant<Schema> mutant : mutants) {
+            mutant.setMutantProducer(this);
+        }
+
+        return mutants;
+    }
+}
