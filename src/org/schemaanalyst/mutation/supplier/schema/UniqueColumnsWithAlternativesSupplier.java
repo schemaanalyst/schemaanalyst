650b36e72866458f7b5a398e3527621bc6e58b4c
Chris Wright <aca08cw@sheffield.ac.uk>
2013-12-13 13:15:38 +0000

    Fixing duplicate in unique bug

--- src/org/schemaanalyst/mutation/supplier/schema/UniqueColumnsWithAlternativesSupplier.java
+++ src/org/schemaanalyst/mutation/supplier/schema/UniqueColumnsWithAlternativesSupplier.java
@@ -1,7 +1,9 @@
 package org.schemaanalyst.mutation.supplier.schema;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.schemaanalyst.mutation.supplier.SolitaryComponentSupplier;
 import org.schemaanalyst.sqlrepresentation.Column;
@@ -13,37 +15,39 @@ import org.schemaanalyst.util.tuple.Pair;
  * {@link org.schemaanalyst.sqlrepresentation.constraint.UniqueConstraint},
  * along with alternative columns from the same table (not used in the
  * <tt>UNIQUE</tt> constraint) on which the constraint is defined.
- * 
+ *
  * @author Phil McMinn
- * 
+ *
  */
 public class UniqueColumnsWithAlternativesSupplier extends
-		SolitaryComponentSupplier<UniqueConstraint, Pair<List<Column>>> {
+        SolitaryComponentSupplier<UniqueConstraint, Pair<List<Column>>> {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void putComponentBackInDuplicate(Pair<List<Column>> columnListPair) {
-		List<Column> columns = columnListPair.getFirst();
-		currentDuplicate.setColumns(columns);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void putComponentBackInDuplicate(Pair<List<Column>> columnListPair) {
+        // Use set to ensure no duplicates
+        Set<Column> set = new LinkedHashSet<>(columnListPair.getFirst());
+        List<Column> columns = new ArrayList<>(set);
+        currentDuplicate.setColumns(columns);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected Pair<List<Column>> getComponent(UniqueConstraint uniqueConstraint) {
-		List<Column> columns = uniqueConstraint.getColumns();
-		List<Column> alternatives = new ArrayList<>();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Pair<List<Column>> getComponent(UniqueConstraint uniqueConstraint) {
+        List<Column> columns = uniqueConstraint.getColumns();
+        List<Column> alternatives = new ArrayList<>();
 
-		for (Column column : uniqueConstraint.getTable().getColumns()) {
-			if (!columns.contains(column)) {
-				alternatives.add(column);
-			}
-		}
-
-		return new Pair<>(columns, alternatives);
-	}
+        for (Column column : uniqueConstraint.getTable().getColumns()) {
+            if (!columns.contains(column)) {
+                alternatives.add(column);
+            }
+        }
+        System.out.println("Columns: " + columns + " Alternatives: " + alternatives);
+        return new Pair<>(columns, alternatives);
+    }
 
 }
