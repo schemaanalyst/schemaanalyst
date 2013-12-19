89b5255eca91a28cbdc0d20b498bd2c9894b9348
Nathaniel Blake <blaken@allegheny.edu>
2013-12-19 16:06:10 -0500

    Updating experiment.h comments and runpath; adding supplemental results analysis code

--- spot/experiment.R
+++ spot/experiment.R
@@ -1,28 +1,67 @@
-############################################################
-# SPOT Experiment for parameter tuning with SchemaAnalyst
+#####################################################################
+# experiment.R
 #
-# The experiment design is that SPOT generates a set of
-# parameters for GenerateResultsFromGenerator, while the user
-# specifies the case study and the generator. At this point,
-# the generation is run with the generated parameters. The
-# execution time of the generation is used as an optional
-# secondary optimization target.
+# Experiment for optimizing the runtime parameters of SchemaAnalyst
+# using the Sequential Parameter Optimization Toolkit (SPOT).
 #
-# Next, Original mutation analysis is run and the inverse
-# of the mutation score is recorded and used as the first
-# (or only) optimization target.
+# The experiment is designed around the general two-phase process
+# of using SchemaAnalyst by (1) generating a series of database 
+# interactions (i.e., SQL statements) against an initial schema
+# and then (2) running mutation analysis using this initial set of 
+# database interactions to kill (optimally, at least) a series of 
+# mutant schemas.
+# 
+# Here, SPOT is used to generate a variety of configurations that
+# take into account four parameters of the database interaction
+# generation phase: (1) satisfy rows, (2) negate rows,
+# (3) random profile, and (4) random seed, each across
+# a series of values defined in the SPOT region of interest (ROI)
+# declaration that is part of expConfig().
 #
-# Usage:
-# config <- expConfig(num_evaluations, use_generationtime)
-# res <- expRun(config, "schema_name", "datagenerator")
+# In each evaluation, the SPOT-generated configuration is then used
+# to generate a set of database interactions, at which point we
+# run mutation analysis using the Original technique (which,
+# despite being relatively slowest, is also most comprehensive).
 #
-# To see the best parameter configurations, print
+# During these steps, both the runtime of the generation phase
+# and the mutant score resulting from the analysis phase are
+# recorded and provided to SPOT as optimization targets
+# (although the useGenerationTime option to expConfig()
+# does allow for the use of mutationScore alone as the
+# optimization target). In particular, note that SPOT minimizes the
+# optimization targets, so we actually provide it with the inverse
+# mutation score (1 - mutationScore).
+# 
+# After completing the specified number of evaluations, the results
+# of the SPOT analysis are returned by the expRun() function as
+# well as stored in the variable res and then saved to an .RData
+# file in the spot/results/{DB_NAME}/ directory.
+#
+# ---- Usage:---------------------------------------------------
+# Note: It is expected that this code will be run from the 
+# project directory root for SchemaAnalyst; otherwise, there 
+# will be classpath issues. From here, the functions can
+# be loaded by running
+# source("spot/experiment.R")
+#
+# An experiment can be configured and run with the following:
+# config <- expConfig(numEvaluations, useGenerationTime)
+# res <- expRun(config, "{SCHEMA_NAME}", "{GENERATOR}")
+#
+# To see the best determined parameter configurations, print
 # res$alg.currentBest
 #
-# To see all configurations, print
+# To see all tested configurations, print
 # res$alg.currentResult
 #
-# import the SPOT package
+# Saved results can be loaded in the current R session with
+# load("spot/results/{DB_NAME}/{RESULTS_FILE_NAME}.RData")
+# at which point the results are stored in the variable res.
+# --------------------------------------------------------------
+#
+# Authors: Nathaniel Blake & Colin Soleim, Allegheny College
+#####################################################################
+
 require(SPOT)
 
 # Define experiment variables
@@ -33,7 +72,7 @@ MAX_NEGATE_ROWS <- 10
 
 # Define the configuration function (substitute for configuration files).
 # -- Takes as a parameter the maximum number of evaluations for the SPOT experiment
-expConfig <- function(num_evals, use_generationtime=FALSE) {
+expConfig <- function(numEvals, useGenerationTime=FALSE) {
 	# Set Region Of Interest; variables are as follows:
 	# X1 - negaterows
 	# X2 - satisfyrows
@@ -45,8 +84,8 @@ expConfig <- function(num_evals, use_generationtime=FALSE) {
 				   lower = c(1, 1, 1, 0), # minimum values
 				   upper = c(MAX_NEGATE_ROWS, MAX_SATISFY_ROWS, 2, 9999999), # max values
 				   type = c("INT", "INT", "FACTOR", "INT"), # variable types
-				   # inexplicably, variable labels cause an error (t.default(x) argument is not a matrix)
-				   # leave them out for now
+				   # Inexplicably, variable labels cause an error (t.default(x) argument is not a matrix),
+				   # so leave them out for now.
 				   #varnames = c("negaterows", "satisfyrows", "randomprofile", "randomseed"), # variable labels
 				   dimROI = 4
 				)
@@ -63,10 +102,7 @@ expConfig <- function(num_evals, use_generationtime=FALSE) {
 		)
 		randomseed = pars[4]
 
-		# go up a directory to the project root in order for java calls to work
-		setwd("..")
-
-		# generate the set of INSERT statements using the current parameters
+		# generate the set of database interaction statements using the current parameters
 		genCallString <- paste("java -Xmx3G -cp build/:lib/*",
 							   " org.schemaanalyst.mutation.analysis.util.GenerateResultsFromGenerator",
 							   " parsedcasestudy.", casestudy, 
@@ -81,37 +117,34 @@ expConfig <- function(num_evals, use_generationtime=FALSE) {
 		analysisCallString <- paste("java -Xmx3G -cp build/:lib/*",
 									" org.schemaanalyst.mutation.analysis.technique.Original",
 									" parsedcasestudy.", casestudy, " 1", 
-									" --outputfolder=spot/", sep = "")
+									" --outputfolder=spot/analyses/", sep = "")
 		system(analysisCallString, intern=FALSE);
 
-		# return to the spot directory
-		setwd("spot")
-
 		# the above java code will output to a csv file; read in the most recent mutation score
-		scores <- read.csv(paste("parsedcasestudy.", casestudy, ".dat", sep=""))
-		mutation_numerator <- tail(scores$scorenumerator, n=1)
-		mutation_denominator <- tail(scores$scoredenominator, n=1)
-		mutation_score <- mutation_numerator / mutation_denominator
+		scores <- read.csv(paste("spot/analyses/parsedcasestudy.", casestudy, ".dat", sep=""))
+		mutationNumerator <- tail(scores$scorenumerator, n=1)
+		mutationDenominator <- tail(scores$scoredenominator, n=1)
+		mutationScore <- mutationNumerator / mutationDenominator
 		# invert the mutation score (optimization minimizes this value)
-		inverse_mutation_score <- 1 - mutation_score
+		inverseMutationScore <- 1 - mutationScore
 
-		if (use_generationtime) {
+		if (useGenerationTime) {
 			# read in the generation execution time
-			generation_costs <- read.csv("generationCosts.dat")
-			gen_exec_time <- tail(generation_costs$timetaken, n=1)
+			generationCosts <- read.csv("spot/generationCosts.dat")
+			genExecTime <- tail(generationCosts$timetaken, n=1)
 
 			# this line will return a multi-variable optimization target that includes
 			# both the mutation score and the execution time of AVM
-			return(c(inverse_mutation_score, gen_exec_time))
+			return(c(inverseMutationScore, genExecTime))
 		} 
 		else { # we'll only optimize the mutation score
-			return(inverse_mutation_score)
+			return(inverseMutationScore)
 		}
 
 	}
 
 	# set SPOT result column titles according to whether we're using multiple optimization targets
-	if (use_generationtime) {
+	if (useGenerationTime) {
 		resultColumn <- c("inverse-mutation-score", "generation-time")
 	}
 	else
@@ -123,15 +156,14 @@ expConfig <- function(num_evals, use_generationtime=FALSE) {
 				alg.resultColumn=resultColumn, # defined conditionally above
 				alg.roi = roi,
 				# Total number of evaluations
-				auto.loop.nevals = num_evals, # expConfig() function parameter
+				auto.loop.nevals = numEvals, # expConfig() function parameter
 				init.design.func = "spotCreateDesignLhd",
-				# number of separate designs to check--
-				# bigger will get better results but take longer
+				# number of separate initial designs to check
 				init.design.size = 10,
 				# number of runs per parameter configuration--
-				# 1 should be fine since this is deterministic
+				# 1 works efficiently since this is deterministic
 				init.design.repeats = 1,
-				spot.filemode = TRUE,
+				spot.filemode = FALSE,
 				seq.predictionModel.func = "spotPredictRandomForest",
 				spot.seed=123,
 				spot.ocba=FALSE
@@ -140,8 +172,10 @@ expConfig <- function(num_evals, use_generationtime=FALSE) {
 	return(config)
 }
 
-# define a function that initiates a SPOT experiment
-# example arguments:
+# Function that initiates the SPOT experiment and stores the result in
+# a variable res, saved in an RData file to spot/results/{DB_NAME}/.
+#
+# Example arguments:
 # 	casestudy="NistDML183"
 # 	generator="alternatingValue"
 expRun <- function(config, casestudy, generator) {
@@ -149,9 +183,14 @@ expRun <- function(config, casestudy, generator) {
 	res <- spot(spotConfig=config, casestudy=casestudy, generator=generator)
 
 	# save the results to a file
-	directory = paste("results/", DATABASE, "/", sep="")
-	filename = paste(casestudy, generator, config$auto.loop.nevals, ".RData", sep="-")
-	save(res, file=paste(directory, filename, sep=""))
+	outputDir <- paste0("spot/results/", DATABASE, "/")
+	filename <- paste(casestudy, generator, config$auto.loop.nevals, sep="-")
+	filePath <- paste0(outputDir, filename, ".RData")
+	# ensure that the output directory exists; if not, create it
+	if (file.exists(outputDir) || dir.create(outputDir, recursive=TRUE))
+		save(res, file=filePath)
+	else # in this case, the directory doesn't exist and we failed to create it
+		print(paste0("Error: Unable to save results to '",filePath,"'."))
 
 	return(res)
 }
