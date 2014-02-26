556bbf6b85480a5ad9e5c54856e08014cc43f84e
Phil McMinn <p.mcminn@sheffield.ac.uk>
2014-02-26 08:29:54 +0000

    Minor ammendment's to Marco's tests so that my IDE doesn't complain :-)

--- src/org/schemaanalyst/test/faultlocalization/TarantulaTest.java
+++ src/org/schemaanalyst/test/faultlocalization/TarantulaTest.java
@@ -14,7 +14,7 @@ public class TarantulaTest {
 		Calculator.calculate(r);
 //		System.out.println("Score is: " + r.getScore());
 		double score = (double) Math.round(r.getScore() * 100)/100;
-		assertEquals(0.5, score);
+		assertEquals(0.5, score, 0.01);
 	}
 
 	@Test
@@ -22,7 +22,7 @@ public class TarantulaTest {
 		TarantulaResultMatrixRow r = new TarantulaResultMatrixRow("second Constraint" , "second mutant", 2, 5, 4, 2, false);
 		Calculator.calculate(r);
 		double score = (double) Math.round(r.getScore() * 100)/100;
-		assertEquals(0.56, score);
+		assertEquals(0.56, score, 0.01);
 	}
 
 	@Test
@@ -30,7 +30,7 @@ public class TarantulaTest {
 		TarantulaResultMatrixRow r = new TarantulaResultMatrixRow("first Constraint" , "first mutant", 2, 5, 3, 2, false);
 		Calculator.calculate(r);
 		double score = (double) Math.round(r.getScore() * 100)/100;
-		assertEquals(0.63, score);
+		assertEquals(0.63, score, 0.01);
 	}
 
 
