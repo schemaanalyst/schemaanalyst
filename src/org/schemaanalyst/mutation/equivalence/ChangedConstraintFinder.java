22f9101a159119931bddee57d735fa75bc093639
Chris Wright <aca08cw@sheffield.ac.uk>
2014-03-07 10:50:52 +0000

    Adding "constraint-diff" for Marco

---
+++ src/org/schemaanalyst/mutation/equivalence/ChangedConstraintFinder.java
@@ -0,0 +1,42 @@
+
+package org.schemaanalyst.mutation.equivalence;
+
+import org.schemaanalyst.sqlrepresentation.Schema;
+import org.schemaanalyst.sqlrepresentation.constraint.Constraint;
+
+/**
+ * <p>
+ * Class that can determine which constraint has been affected by a change 
+ * between two versions of a schema.
+ * </p>
+ * 
+ * <p>
+ * Note: This does not support differences in foreign key constraints.
+ * </p>
+ * 
+ * @author Chris J. Wright
+ */
+public class ChangedConstraintFinder extends ChangedElementFinder {
+    public static Constraint getDifferentConstraint(Schema first, Schema second) {
+        Constraint diffC = findDifferent(new PrimaryKeyEquivalenceChecker(true), first.getPrimaryKeyConstraints(), second.getPrimaryKeyConstraints());
+        if (diffC != null) {
+            return diffC;
+        }
+
+        diffC = findDifferent(new UniqueEquivalenceChecker(true), first.getUniqueConstraints(), second.getUniqueConstraints());
+        if (diffC != null) {
+            return diffC;
+        }
+
+        diffC = findDifferent(new CheckEquivalenceChecker(true), first.getCheckConstraints(), second.getCheckConstraints());
+        if (diffC != null) {
+            return diffC;
+        }
+
+        diffC = findDifferent(new NotNullEquivalenceChecker(true), first.getNotNullConstraints(), second.getNotNullConstraints());
+        if (diffC != null) {
+            return diffC;
+        }
+        return null;
+    }
+}
--- src/org/schemaanalyst/mutation/equivalence/ChangedTableFinder.java
+++ src/org/schemaanalyst/mutation/equivalence/ChangedTableFinder.java
@@ -2,7 +2,6 @@
  */
 package org.schemaanalyst.mutation.equivalence;
 
-import java.util.List;
 import org.schemaanalyst.sqlrepresentation.*;
 import org.schemaanalyst.sqlrepresentation.constraint.*;
 
@@ -18,7 +17,7 @@ import org.schemaanalyst.sqlrepresentation.constraint.*;
  *
  * @author Chris J. Wright
  */
-public class ChangedTableFinder {
+public class ChangedTableFinder extends ChangedElementFinder {
 
     /**
      * Gets the table which is different between two schemas (assuming only one
@@ -53,30 +52,6 @@ public class ChangedTableFinder {
         if (diffC != null) {
             return diffC.getTable();
         }
-        return null;
-    }
-
-    /**
-     * Gets the first element from second that is not equivalent to the element
-     * in the same index in first.
-     *
-     * @param <T> The type of element
-     * @param checker The equivalence checker
-     * @param first The first list of elements
-     * @param second The second list of elements
-     * @return The different element, or null if there is not one
-     */
-    protected static <T> T findDifferent(EquivalenceChecker<T> checker, List<T> first, List<T> second) {
-        List<T> secondNotFirst = checker.subtract(second, first);
-        if (secondNotFirst.size() > 0) {
-            return secondNotFirst.get(0);
-        }
-
-        List<T> firstNotSecond = checker.subtract(first, second);
-        if (firstNotSecond.size() > 0) {
-            return firstNotSecond.get(0);
-        }
-
         return null;
     }
 }
