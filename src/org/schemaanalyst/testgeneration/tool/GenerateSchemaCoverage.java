fdb28312e0119ad9cedcb7490327367466bf28aa
Phil McMinn <p.mcminn@sheffield.ac.uk>
2014-03-24 10:48:51 +0000

    Turned GenerateSchemaCoverage into a proper tool class

--- src/org/schemaanalyst/testgeneration/tool/GenerateSchemaCoverage.java
+++ src/org/schemaanalyst/testgeneration/tool/GenerateSchemaCoverage.java
@@ -26,7 +26,7 @@ import static org.schemaanalyst.util.java.JavaUtils.JAVA_FILE_SUFFIX;
  * Created by phil on 21/01/2014.
  */
 
-@RequiredParameters("schema criterion datagenerator dbms")
+@RequiredParameters("schema dbms criterion datagenerator")
 public class GenerateSchemaCoverage extends Runner {
 
     @Parameter("The name of the schema to use.")
@@ -41,8 +41,11 @@ public class GenerateSchemaCoverage extends Runner {
     @Parameter("The name of the DBMS to use.")
     protected String dbms;
 
-    @Parameter("Whether a test suite should be written or not.")
-    protected boolean buildTestSuite = false;
+    @Parameter("The class name of the generated test suite (if not specified class name will be 'TestSchema' where schema is the name of the schema under test")
+    protected String classname = "";
+
+    @Parameter("The package into which the built test suite is placed")
+    protected String packagename = "generatedtest";
 
     @Override
     protected void task() {
@@ -61,80 +64,27 @@ public class GenerateSchemaCoverage extends Runner {
                 dataGeneratorObject);
         TestSuite testSuite = testSuiteGenerator.generate();
 
-        printReport(schemaObject, criterionObject, testSuite, testSuiteGenerator.getFailedTestCases());
+        // execute each test case to see what the DBMS result is for each row generated (accept / row)
+        TestCaseExecutor executor = new TestCaseExecutor(
+                schemaObject,
+                dbmsObject,
+                new DatabaseConfiguration(),
+                new LocationsConfiguration());
+        executor.execute(testSuite);
 
-        if (buildTestSuite) {
-            // execute each test case to see what the DBMS result is for each row generated (accept / row)
-            TestCaseExecutor executor = new TestCaseExecutor(
-                    schemaObject,
-                    dbmsObject,
-                    new DatabaseConfiguration(),
-                    new LocationsConfiguration());
-            executor.execute(testSuite);
-
-            // write JUnit test suite to file
-            writeTestSuite(schemaObject, dbmsObject, testSuite, "generatedtest");
-        }
-    }
-
-    private void printReport(Schema schemaObject,
-                             CoverageCriterion criterionObject,
-                             TestSuite testSuite,
-                             List<TestCase> failedTestCases) {
-
-        // print out each test suite test case
-        //System.out.println("SUCCESSFUL TEST CASES:");
-        //for (TestCase testCase : testSuite.getTestCases()) {
-        //    System.out.println("\n" + testCase);
-        //}
-
-        //// print out each failed test case
-        System.out.println("FAILED TEST CASES:");
-        for (TestCase testCase : failedTestCases) {
-            System.out.println("\n" + testCase);
+        // write JUnit test suite to file
+        if (classname.equals("")) {
+            classname = "Test" + schemaObject.getName();
         }
 
-        System.out.println("\nTEST SUITE STATS:");
-        System.out.println("Number of failed test cases: " + failedTestCases.size());
-        System.out.println("Number of successful test cases: " + testSuite.getNumTestCases());
-        System.out.println("Number of inserts: " + testSuite.getNumInserts());
-        System.out.println("Number of evaluations: " + testSuite.getNumEvaluations());
-        System.out.println("Average No. of evaluations: " + testSuite.getAvNumEvaluations());
-
-        for (CoverageCriterion criterion : CoverageCriterionFactory.allCriteria()) {
-            String name = criterion.getName();
-            String starred = "";
-            if (name.equals(criterionObject.getName())) {
-                starred = " (*)";
-            }
-
-            CoverageReport coverageReport =
-                    new CoverageReport(testSuite, criterion.generateRequirements(schemaObject));
-            System.out.println(name + starred + ": " + coverageReport.getCoverage());
-
-            //if (printUncoveredPredicates) {
-                List<Predicate> uncovered = coverageReport.getUncoveredRequirements();
-                //if (uncovered.size() > 0) {
-                //    System.out.println("Uncovered predicates: ");
-                //    for (Predicate predicate : uncovered) {
-                //        System.out.println(predicate.getPurposes() + ": " + predicate);
-                //    }
-                //}
-            //}
-        }
-    }
-
-    private void writeTestSuite(Schema schema, DBMS dbms, TestSuite testSuite, String packageName) {
-        String className = "Test" + schema.getName();
-
-        String javaCode = new TestSuiteJavaWriter(schema, dbms, testSuite)
-                .writeTestSuite(packageName, className);
+        String javaCode = new TestSuiteJavaWriter(schemaObject, dbmsObject, testSuite)
+                .writeTestSuite(packagename, classname);
 
         File javaFile = new File(locationsConfiguration.getSrcDir()
-                + "/" + packageName + "/" + className + JAVA_FILE_SUFFIX);
+                + "/" + packagename + "/" + classname + JAVA_FILE_SUFFIX);
         try (PrintWriter fileOut = new PrintWriter(javaFile)) {
             fileOut.println(javaCode);
-            System.out.println("\n[JUnit test suite written to " + javaFile + "]");
+            System.out.println("JUnit test suite written to " + javaFile);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
