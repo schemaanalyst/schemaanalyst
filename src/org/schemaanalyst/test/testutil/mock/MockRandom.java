a155831b4ba0fa03ab724ae86bc9619f02977f60
Phil McMinn <p.mcminn@sheffield.ac.uk>
2014-04-10 10:17:12 +0100

    Changes to random

--- src/org/schemaanalyst/test/testutil/mock/MockRandom.java
+++ src/org/schemaanalyst/test/testutil/mock/MockRandom.java
@@ -55,4 +55,9 @@ public class MockRandom extends Random {
     public double nextDouble() {
         return nextInt() / 100.0;
     }
+
+    @Override
+    public long nextLong() {
+        return (long) nextInt();
+    }
 }
