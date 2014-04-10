25be24b4a429939b7ffea1469de6433fe416b941
Phil McMinn <p.mcminn@sheffield.ac.uk>
2014-04-10 10:21:44 +0100

    Re-added GenerateSeeds.java following a merge conflict

---
+++ src/paper/datagenerationjv/GenerateSeeds.java
@@ -0,0 +1,25 @@
+package paper.datagenerationjv;
+
+import org.schemaanalyst.util.random.Random;
+import org.schemaanalyst.util.random.SimpleRandom;
+
+/**
+ * Created by phil on 26/03/2014.
+ */
+public class GenerateSeeds {
+
+    public static void main(String... args) {
+
+        final int NUM_REQUIRED = 30;
+
+        Random random = new SimpleRandom(System.currentTimeMillis());
+        System.out.println();
+
+        for (int i=0; i < NUM_REQUIRED; i++) {
+            System.out.print("\t\t" + random.nextLong() + "L");
+            if (i < NUM_REQUIRED-1) {
+                System.out.println(",");
+            }
+        }
+    }
+}
