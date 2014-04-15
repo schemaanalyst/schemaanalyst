bafc8d8555f65cfd1fa12d463a79a82c40776e29
Chris Wright <aca08cw@sheffield.ac.uk>
2014-04-15 16:00:35 +0100

    Manual fix for bug in ArtistSimilarity - multiple constraints with same name are rejected by Postgres

--- src/parsedcasestudy/ArtistSimilarity.java
+++ src/parsedcasestudy/ArtistSimilarity.java
@@ -18,13 +18,13 @@ public class ArtistSimilarity extends Schema {
 
 		Table tableArtists = this.createTable("artists");
 		tableArtists.createColumn("artist_id", new TextDataType());
-		this.createPrimaryKeyConstraint("null", tableArtists, tableArtists.getColumn("artist_id"));
+		this.createPrimaryKeyConstraint(tableArtists, tableArtists.getColumn("artist_id"));
 
 		Table tableSimilarity = this.createTable("similarity");
 		tableSimilarity.createColumn("target", new TextDataType());
 		tableSimilarity.createColumn("similar", new TextDataType());
-		this.createForeignKeyConstraint("null", tableSimilarity, tableSimilarity.getColumn("target"), tableArtists, tableArtists.getColumn("artist_id"));
-		this.createForeignKeyConstraint("null", tableSimilarity, tableSimilarity.getColumn("similar"), tableArtists, tableArtists.getColumn("artist_id"));
+		this.createForeignKeyConstraint(tableSimilarity, tableSimilarity.getColumn("target"), tableArtists, tableArtists.getColumn("artist_id"));
+		this.createForeignKeyConstraint(tableSimilarity, tableSimilarity.getColumn("similar"), tableArtists, tableArtists.getColumn("artist_id"));
 	}
 }
 
