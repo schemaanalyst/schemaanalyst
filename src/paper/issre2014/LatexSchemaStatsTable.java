7fd4d52431eed34a7ac8b8ea92f45e63885aff67
Chris Wright <aca08cw@sheffield.ac.uk>
2014-04-15 15:51:10 +0100

    Adding possible schema list for ISSRE2014

---
+++ src/paper/issre2014/LatexSchemaStatsTable.java
@@ -0,0 +1,45 @@
+package paper.issre2014;
+
+import org.schemaanalyst.sqlrepresentation.Schema;
+import paper.util.SchemaStatsTable;
+import parsedcasestudy.*;
+
+public class LatexSchemaStatsTable extends SchemaStatsTable {
+
+    public static Schema[] schemas = {
+        new ArtistSimilarity(),
+        new BookTown(),
+        new Cloc(),
+        new Flights(),
+        new IsoFlav_R2Repaired(),
+        new JWhoisServer(),
+        new NistDML183(),
+        new RiskIt(),
+        new UnixUsage(),
+        new WordNet()
+    };
+
+    public LatexSchemaStatsTable() {
+        super(" & ", " \\\\\n");
+    }
+
+//    @Override
+//    protected void writeHeader(StringBuffer table) {
+//        table.append("%!TEX root=mut13-schemata.tex\n");
+//    }
+    @Override
+    protected void writeFooter(StringBuffer table,
+            int totalNumTables, int totalNumColumns, // int totalUniqueColumnTypes,
+            int totalNumChecks, int totalNumForeignKeys, int totalNumNotNulls,
+            int totalNumPrimaryKeys, int totalNumUniques) {
+        table.append("\\midrule \n");
+
+        writeRow(table, "{\\bf Total}", totalNumTables, totalNumColumns, // totalUniqueColumnTypes, 
+                totalNumChecks, totalNumForeignKeys, totalNumNotNulls, totalNumPrimaryKeys, totalNumUniques);
+    }
+
+    public static void main(String[] args) {
+        LatexSchemaStatsTable table = new LatexSchemaStatsTable();
+        System.out.println(table.write(schemas));
+    }
+}
