544e8885f2f17e49f7bb4441ef214ff1eef522ec
Chris Wright <aca08cw@sheffield.ac.uk>
2014-08-11 15:16:30 +0100

    Writer for producing constraints as CHECKs

---
+++ src/org/schemaanalyst/sqlwriter/ConstraintAsCheckSQLWriter.java
@@ -0,0 +1,127 @@
+
+package org.schemaanalyst.sqlwriter;
+
+import java.util.Iterator;
+import java.util.List;
+import org.schemaanalyst.sqlrepresentation.*;
+import org.schemaanalyst.sqlrepresentation.constraint.*;
+import org.schemaanalyst.sqlrepresentation.expression.*;
+
+
+/**
+ * Writer to transform all types of constraints into CHECK constraints.
+ * 
+ * @author Chris J. Wright
+ */
+public class ConstraintAsCheckSQLWriter extends ConstraintSQLWriter {
+    private final String INACTIVE_MUTANT;
+
+    public ConstraintAsCheckSQLWriter(String mutantId) {
+        INACTIVE_MUTANT = "NOT active_mutant(" + mutantId + "::text)";
+    }
+
+    @Override
+    public String writeCheck(CheckConstraint check) {
+        Expression expression = check.getExpression();
+        String sql = expressionSQLWriter.writeExpression(expression);
+        return "CHECK (" + INACTIVE_MUTANT + " OR (" + sql + "))";
+    }
+
+    @Override
+    public String writePrimaryKey(PrimaryKeyConstraint primaryKey) {
+        List<Column> columns = primaryKey.getColumns();
+        // Must be the active mutant
+        String sql = "CHECK (" + INACTIVE_MUTANT + " OR (";
+        // Must be not null
+        for (Column column : columns) {
+            sql += satisfyNotNull(column);
+            sql += " AND ";
+        }
+        // Must satisfy pk rules
+        sql += satisfyPrimaryKey(primaryKey.getTable(), columns);
+        sql += "))";
+        return sql;
+    }
+
+    @Override
+    public String writeUnique(UniqueConstraint unique) {
+        // Must be the active mutant
+        String sql = "CHECK (" + INACTIVE_MUTANT + " OR (";
+        // Must have at least one null valued column...
+        for (Column column : unique.getColumns()) {
+            sql += "NOT " + satisfyNotNull(column);
+            sql += " OR ";
+        }
+        // ...or satisfy unique rules
+        sql += satisfyUnique(unique.getTable(), unique.getColumns());
+        sql += "))";
+        return sql;
+    }
+
+    @Override
+    public String writeNotNull(NotNullConstraint notNull) {
+        String sql = "CHECK (" + INACTIVE_MUTANT + " OR (";
+        sql += satisfyNotNull(notNull.getColumn());
+        sql += "))";
+        return sql;
+    }
+
+    @Override
+    public String writeForeignKey(ForeignKeyConstraint foreignKey) {
+//        CHECK (NOT satisfy_notnull(c::text) OR NOT satisfy_notnull(d::text) OR satisfy_fk('one'::text,'{a,b}'::text[],ARRAY[c,d]::text[]));
+        List<Column> columns = foreignKey.getColumns();
+        List<Column> refColumns = foreignKey.getReferenceColumns();
+        // Must be the active mutant
+        String sql = "CHECK (" + INACTIVE_MUTANT + " OR (";
+        // Either a column is null...
+        for (Column column : columns) {
+            sql += "NOT " + satisfyNotNull(column) + " OR ";
+        }
+        // ...or the fk rules are satisfied
+        sql += satisfyFk(foreignKey.getReferenceTable(), columns, refColumns);
+        sql += "))";
+        return sql;
+    }
+    
+    private static String satisfyPrimaryKey(Table table, List<Column> columns) {
+        String colNames = columnsToNamesString(columns);
+        String sql = "satisfy_pk('" + table.getName() + "'::text,";
+        sql += "'{" + colNames + "}'::text[],";
+        sql += "ARRAY[" + colNames + "]::text[])";
+        return sql;
+    }
+    
+    private static String satisfyUnique(Table table, List<Column> columns) {
+        String colNames = columnsToNamesString(columns);
+        String sql = "satisfy_unique('" + table.getName() + "'::text,";
+        sql += "'{" + colNames + "}'::text[],";
+        sql += "ARRAY[" + colNames + "]::text[])";
+        return sql;
+    }
+    
+    private static String satisfyNotNull(Column column) {
+        return "satisfy_notnull(" + column.getName() + "::text)";
+    }
+    
+    private static String satisfyFk (Table refTable, List<Column> columns, List<Column> refColumns) {
+        String colNames = columnsToNamesString(columns);
+        String refColNames = columnsToNamesString(refColumns);
+        String sql = "satisfy_fk('" + refTable.getName() + "'::text,";
+        sql += "'{" + refColNames + "}'::text[],";
+        sql += "ARRAY[" + colNames + "]::text[])";
+        return sql;
+    }
+    
+    private static String columnsToNamesString(List<Column> columns) {
+        String colNames = "";
+        Iterator<Column> iter = columns.iterator();
+        while (iter.hasNext()) {
+            colNames += iter.next().getName();
+            if (iter.hasNext()) {
+                colNames += ",";
+            }
+        }
+        return colNames;
+    }
+    
+}
