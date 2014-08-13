2701a925feb95b92a3c8e8e5c39de7cf264432b6
Chris Wright <aca08cw@sheffield.ac.uk>
2014-08-13 11:39:56 +0100

    Commit prior to major overhaul of SQL functions

--- scripts/checks/functions.sql
+++ scripts/checks/functions.sql
@@ -4,26 +4,26 @@ $$
 DECLARE
 result integer;
 BEGIN
-EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %s WHERE %s=%s))::int', _table,_column,_value::text) INTO result;
+EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %I WHERE %I=%s))::int', _table,_column,_value::text) INTO result;
 RETURN NOT result::boolean;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- PRIMARY KEY constraint (multi-column)
 CREATE OR REPLACE FUNCTION satisfy_pk(_table text, _columns text[], _values text[]) RETURNS boolean AS
 $$
 DECLARE 
 result integer;
-statement text = 'SELECT (EXISTS (SELECT 1 FROM ' || _table || ' WHERE ';
+statement text = 'SELECT (EXISTS (SELECT 1 FROM ' || quote_ident(_table) || ' WHERE ';
 first boolean = true;
 BEGIN
 FOR i IN array_lower(_columns,1)..array_upper(_columns,1) LOOP
 IF first THEN
-statement = statement || _columns[i] || '=' || _values[i];
+statement = statement || quote_ident(_columns[i]) || '=' || _values[i];
 first = false;
 ELSE
-statement = statement || ' AND '|| _columns[i] || '=' || _values[i];
+statement = statement || ' AND '|| quote_ident(_columns[i]) || '="' || _values[i] || '"';
 END IF;
 END LOOP;
 statement = statement || '))::int';
@@ -31,7 +31,7 @@ EXECUTE statement INTO result;
 RETURN NOT result::boolean;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- UNIQUE constraint
 CREATE OR REPLACE FUNCTION satisfy_unique(_table text, _column text, _value text) RETURNS boolean AS
@@ -39,26 +39,26 @@ $$
 DECLARE
 result integer;
 BEGIN
-EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %s WHERE %s=%s))::int', _table,_column,_value::text) INTO result;
+EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %I WHERE %I=%s))::int', _table,_column,_value::text) INTO result;
 RETURN NOT result::boolean;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- UNIQUE constraint multi-column
 CREATE OR REPLACE FUNCTION satisfy_unique(_table text, _columns text[], _values text[]) RETURNS boolean AS
 $$
 DECLARE 
 result integer;
-statement text = 'SELECT (EXISTS (SELECT 1 FROM ' || _table || ' WHERE ';
+statement text = 'SELECT (EXISTS (SELECT 1 FROM ' || quote_ident(_table) || ' WHERE ';
 first boolean = true;
 BEGIN
 FOR i IN array_lower(_columns,1)..array_upper(_columns,1) LOOP
 IF first THEN
-statement = statement || _columns[i] || '=' || _values[i];
+statement = statement || quote_ident(_columns[i]) || '=' || _values[i];
 first = false;
 ELSE
-statement = statement || ' AND '|| _columns[i] || '=' || _values[i];
+statement = statement || ' AND '|| quote_ident(_columns[i]) || '=' || _values[i];
 END IF;
 END LOOP;
 statement = statement || '))::int';
@@ -66,7 +66,7 @@ EXECUTE statement INTO result;
 RETURN NOT result::boolean;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- FOREIGN KEY constraint
 CREATE OR REPLACE FUNCTION satisfy_fk(_table text, _column text, _value text) RETURNS boolean AS
@@ -74,26 +74,26 @@ $$
 DECLARE
 result integer;
 BEGIN
-EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %s WHERE %s=%s))::int', _table,_column,_value::text) INTO result;
+EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %I WHERE %I=%s))::int', _table,_column,_value::text) INTO result;
 RETURN result::boolean;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- FOREIGN KEY constraint multi-column
 CREATE OR REPLACE FUNCTION satisfy_fk(_table text, _columns text[], _values text[]) RETURNS boolean AS
 $$
 DECLARE 
 result integer;
-statement text = 'SELECT (EXISTS (SELECT 1 FROM ' || _table || ' WHERE ';
+statement text = 'SELECT (EXISTS (SELECT 1 FROM ' || quote_ident(_table) || ' WHERE ';
 first boolean = true;
 BEGIN
 FOR i IN array_lower(_columns,1)..array_upper(_columns,1) LOOP
 IF first THEN
-statement = statement || _columns[i] || '=' || _values[i];
+statement = statement || quote_ident(_columns[i]) || '=' || _values[i];
 first = false;
 ELSE
-statement = statement || ' AND '|| _columns[i] || '=' || _values[i];
+statement = statement || ' AND '|| quote_ident(_columns[i]) || '=' || _values[i];
 END IF;
 END LOOP;
 statement = statement || '))::int';
@@ -101,7 +101,7 @@ EXECUTE statement INTO result;
 RETURN result::boolean;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- NOT NULL constraint
 CREATE OR REPLACE FUNCTION satisfy_notnull(_value text) RETURNS boolean AS
@@ -110,7 +110,7 @@ BEGIN
 RETURN _value IS NOT NULL;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- Active mutant
 CREATE OR REPLACE FUNCTION active_mutant(_id int) RETURNS boolean AS
@@ -118,11 +118,11 @@ $$
 DECLARE
 result integer;
 BEGIN
-EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %s WHERE %s::text=%s::text))::int','schemaanalyst_activemutant','id',_id) INTO result;
+EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %I WHERE %I=%s))::int','schemaanalyst_activemutant','id',_id) INTO result;
 RETURN result::boolean;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- Switch mutant
 CREATE OR REPLACE FUNCTION activate_mutant(_id int) RETURNS int AS
@@ -132,7 +132,7 @@ UPDATE schemaanalyst_activemutant SET id = _id;
 RETURN _id;
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- Assert statement will pass
 CREATE OR REPLACE FUNCTION assertPass(_statement text) RETURNS void AS
@@ -146,7 +146,7 @@ WHEN integrity_constraint_violation OR check_violation THEN
 RAISE EXCEPTION 'TEST CASE FAILED: Expected to pass, but failed';
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
 
 -- Assert statement will fail
 CREATE OR REPLACE FUNCTION assertFail(_statement text) RETURNS void AS
@@ -160,4 +160,4 @@ EXCEPTION
 WHEN integrity_constraint_violation OR check_violation THEN
 END
 $$
-LANGUAGE 'plpgsql';
+LANGUAGE plpgsql;
