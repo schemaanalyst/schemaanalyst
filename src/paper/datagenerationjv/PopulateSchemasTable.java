e89444d2e16d6e6851fc733d29097bc5f0e1d2d3
Phil McMinn <p.mcminn@sheffield.ac.uk>
2014-08-16 12:54:54 +0100

    Added PopulateSchemaTables script

--- src/paper/datagenerationjv/PopulateSchemasTable.java
+++ src/paper/datagenerationjv/PopulateSchemasTable.java
@@ -1,6 +1,12 @@
 package paper.datagenerationjv;
 
 import org.schemaanalyst.sqlrepresentation.Schema;
+import org.schemaanalyst.sqlrepresentation.Table;
+import org.schemaanalyst.sqlrepresentation.constraint.CheckConstraint;
+import org.schemaanalyst.sqlrepresentation.constraint.ForeignKeyConstraint;
+import org.schemaanalyst.sqlrepresentation.constraint.PrimaryKeyConstraint;
+import org.schemaanalyst.sqlrepresentation.constraint.UniqueConstraint;
+import org.schemaanalyst.sqlrepresentation.expression.*;
 
 import java.util.List;
 
@@ -21,8 +27,77 @@ public class PopulateSchemasTable {
 
             Schema schema = instantiateSchema(schemaName);
 
+            int tables = schema.getTables().size();
+            int columns = 0;
+            for (Table table : schema.getTables()) {
+                columns += table.getColumns().size();
+            }
 
+            int pks = schema.getPrimaryKeyConstraints().size();
+            int multiClausePks = 0;
+            for (PrimaryKeyConstraint pk : schema.getPrimaryKeyConstraints()) {
+                if (pk.getColumns().size() > 1) {
+                    multiClausePks ++;
+                }
+            }
 
+            int fks = schema.getForeignKeyConstraints().size();
+            int multiClauseFks = 0;
+            for (ForeignKeyConstraint fk : schema.getForeignKeyConstraints()) {
+                if (fk.getColumns().size() > 1) {
+                    multiClauseFks ++;
+                }
+            }
+
+            int uniques = schema.getUniqueConstraints().size();
+            int multiClauseUniques = 0;
+            for (UniqueConstraint fk : schema.getUniqueConstraints()) {
+                if (fk.getColumns().size() > 1) {
+                    multiClauseUniques ++;
+                }
+            }
+
+            int nns = schema.getNotNullConstraints().size();
+
+            int checks = schema.getCheckConstraints().size();
+            int multiClauseChecks = 0;
+            for (CheckConstraint check : schema.getCheckConstraints()) {
+                Expression expression = check.getExpression();
+                if (expression instanceof AndExpression ||
+                        expression instanceof OrExpression ||
+                        expression instanceof BetweenExpression ||
+                        expression instanceof InExpression) {
+                    multiClauseChecks ++;
+                }
+            }
+
+            String data = "tables=" + tables
+                        + ", columns=" + columns
+                        + ", checks=" + checks
+                        + ", multi_clause_checks=" + multiClauseChecks
+                        + ", fks=" + fks
+                        + ", multi_clause_fks=" + multiClauseFks
+                        + ", nns=" + nns
+                        + ", pks=" + pks
+                        + ", multi_clause_pks=" + multiClausePks
+                        + ", uniques=" + uniques
+                        + ", multi_clause_uniques=" + multiClauseUniques;
+
+            String sql = "UPDATE schemas SET " + data + " WHERE name='" + schemaName + "'";
+
+            System.out.println(sql);
+
+            resultsDatabase.executeInsert(sql);
         }
     }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("No results database file name provided");
+            System.exit(1);
+        }
+
+        new PopulateSchemasTable().populate(args[0]);
+    }
+
 }
