f4508334d15189950580778269726d7c170900b3
Phil McMinn <p.mcminn@sheffield.ac.uk>
2014-10-13 18:11:10 +0100

    Put code back into DirectedRandom to get it to work as before...

--- src/org/schemaanalyst/data/generation/directedrandom/DirectedRandomDataGenerator.java
+++ src/org/schemaanalyst/data/generation/directedrandom/DirectedRandomDataGenerator.java
@@ -1,10 +1,13 @@
 package org.schemaanalyst.data.generation.directedrandom;
 
 import org.schemaanalyst.data.Data;
+import org.schemaanalyst.data.generation.DataGenerationReport;
 import org.schemaanalyst.data.generation.cellinitialization.CellInitializer;
 import org.schemaanalyst.data.generation.cellvaluegeneration.RandomCellValueGenerator;
 import org.schemaanalyst.data.generation.random.RandomDataGenerator;
 import org.schemaanalyst.testgeneration.coveragecriterion.predicate.Predicate;
+import org.schemaanalyst.testgeneration.coveragecriterion.predicate.checker.PredicateChecker;
+import org.schemaanalyst.testgeneration.coveragecriterion.predicate.checker.PredicateCheckerFactory;
 import org.schemaanalyst.util.random.Random;
 
 /**
@@ -13,6 +16,7 @@ import org.schemaanalyst.util.random.Random;
 public class DirectedRandomDataGenerator extends RandomDataGenerator {
 
     private Random random;
+    private PredicateChecker predicateChecker;
     private PredicateFixer predicateFixer;
 
     public DirectedRandomDataGenerator(Random random,
@@ -24,10 +28,27 @@ public class DirectedRandomDataGenerator extends RandomDataGenerator {
     }
 
     @Override
+    public DataGenerationReport generateData(Data data, Data state, Predicate predicate) {
+        initialize(data, state, predicate);
+        boolean success = predicateChecker.check();
+        int evaluations = 0;
+        while (!success && evaluations < maxEvaluations) {
+            attemptFix(data);
+            evaluations ++;
+            success = predicateChecker.check();
+        }
+
+        return new DataGenerationReport(success, evaluations);
+    }
+
+    @Override
     protected void initialize(Data data, Data state, Predicate predicate) {
         super.initialize(data, state, predicate);
+        predicateChecker = PredicateCheckerFactory.instantiate(
+                predicate, true, data, state
+        );
         predicateFixer = PredicateFixerFactory.instantiate(
-                predicate, true, data, state, random, randomCellValueGenerator);
+                predicateChecker, random, randomCellValueGenerator);
     }
 
     @Override
