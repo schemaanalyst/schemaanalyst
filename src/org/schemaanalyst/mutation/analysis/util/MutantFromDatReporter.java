25c41d51e219647b0033d9e7b82685b00d56f1dc
Chris Wright <aca08cw@sheffield.ac.uk>
2015-06-30 11:08:57 +0100

    Adding classes for producing detailedm mutant information from either ids or a .dat file

---
+++ src/org/schemaanalyst/mutation/analysis/util/MutantFromDatReporter.java
@@ -0,0 +1,69 @@
+package org.schemaanalyst.mutation.analysis.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Logger;
+import org.schemaanalyst.util.runner.Parameter;
+import org.schemaanalyst.util.runner.RequiredParameters;
+import org.schemaanalyst.util.runner.Runner;
+
+/**
+ * Prints the detailed description of mutants according to a .dat file.
+ * 
+ * @author Chris J. Wright
+ */
+@RequiredParameters("file")
+public class MutantFromDatReporter extends Runner {
+    
+    @Parameter("The file to read from")
+    protected String file;
+    
+    private static final Logger LOGGER = Logger.getLogger(MutantFromDatReporter.class.getName());
+
+    @Override
+    protected void task() {
+        String dbms = determineDBMS(file);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            // Skip header row
+            reader.readLine();
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                int split = line.indexOf(',');
+                String casestudy = line.substring(0, split).replaceAll("\"", "");
+                String ids = line.substring(split + 1, line.length()).replaceAll("\"", "");
+                
+                String path = file.replaceAll(".dat", "") + "/" + casestudy + "/";
+                new File(path).mkdirs();
+                new MutantReporter().reportToFile(casestudy, dbms, ids, path);
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private String determineDBMS(String file) {
+        String name;
+        if (file.toLowerCase().contains("sqlite")) {
+            name = "SQLite";
+        } else if (file.toLowerCase().contains("postgres")) {
+            name = "Postgres";
+        } else if (file.toLowerCase().contains("hypersql")) {
+            name = "HyperSQL";
+        } else {
+            LOGGER.warning("Unable to detect DBMS from filename. Using configuration value");
+            name = databaseConfiguration.getDbms();
+        }
+        return name;
+    }
+    
+    @Override
+    protected void validateParameters() {
+        // Do nothing
+    }
+    
+    public static void main(String[] args) {
+        new MutantFromDatReporter().run(args);
+    }
+    
+}
---
+++ src/org/schemaanalyst/mutation/analysis/util/MutantReporter.java
@@ -0,0 +1,165 @@
+package org.schemaanalyst.mutation.analysis.util;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.lang3.StringUtils;
+import org.schemaanalyst.dbms.DBMS;
+import org.schemaanalyst.dbms.DBMSFactory;
+import org.schemaanalyst.mutation.Mutant;
+import org.schemaanalyst.mutation.pipeline.MutationPipeline;
+import org.schemaanalyst.mutation.pipeline.MutationPipelineFactory;
+import org.schemaanalyst.sqlrepresentation.Schema;
+import org.schemaanalyst.sqlwriter.SQLWriter;
+import org.schemaanalyst.util.runner.Parameter;
+import org.schemaanalyst.util.runner.RequiredParameters;
+import org.schemaanalyst.util.runner.Runner;
+
+/**
+ * Prints the detailed description of specific numbered mutants of a given schema.
+ * 
+ * @author Chris J. Wright
+ */
+@RequiredParameters("casestudy ids")
+public class MutantReporter extends Runner {
+    
+    /**
+     * The name of the schema to use.
+     */
+    @Parameter("The name of the schema to use.")
+    protected String casestudy;
+    
+    /**
+     * The comma-separated list of mutants to print details of (0 indexed)
+     */
+    @Parameter("The comma-separated list of mutants to print details of (0 indexed)")
+    protected String ids;
+    
+    /**
+     * The mutation pipeline to use to generate mutants.
+     */
+    @Parameter(value = "The mutation pipeline to use to generate mutants.",
+            choicesMethod = "org.schemaanalyst.mutation.pipeline.MutationPipelineFactory.getPipelineChoices")
+    protected String mutationPipeline = "AllOperatorsWithClassifiers";
+
+    private static final Logger LOGGER = Logger.getLogger(MutantReporter.class.getName());
+    
+    @Override
+    protected void task() {
+        report(casestudy, databaseConfiguration.getDbms(), ids);
+    }
+    
+    public void report(String casestudy, String dbms, String ids) {
+        // Instantiate configured options and generate mutants
+        Schema schema = instantiateSchema(casestudy);
+        SQLWriter writer = instantiateDBMSWriter(dbms);
+        List<Mutant<Schema>> mutants = generateMutants(schema, dbms);
+        
+        // Iterate mutants and print
+        for (String idString : ids.split(",")) {
+            int id = Integer.valueOf(idString);
+            if (id < mutants.size()) {
+                Mutant<Schema> mutant = mutants.get(id);
+                print(id,writer,mutant);
+            } else {
+                LOGGER.log(Level.SEVERE, "Mutant ID out of bounds, skipping (index {0} from list of {1})", new Object[] {id, mutants.size()});
+            }
+        }
+    }
+    
+    public void reportToFile(String casestudy, String dbms, String ids, String path) {
+        // Instantiate configured options and generate mutants
+        Schema schema = instantiateSchema(casestudy);
+        SQLWriter writer = instantiateDBMSWriter(dbms);
+        List<Mutant<Schema>> mutants = generateMutants(schema, dbms);
+        
+        // Iterate mutants and print
+        for (String idString : ids.split(",")) {
+            int id = Integer.valueOf(idString);
+            if (id < mutants.size()) {
+                Mutant<Schema> mutant = mutants.get(id);
+                print(id,writer,mutant,path + id);
+            } else {
+                LOGGER.log(Level.SEVERE, "Mutant ID out of bounds, skipping (index {0} from list of {1})", new Object[] {id, mutants.size()});
+            }
+        }
+    }
+
+    @Override
+    protected void validateParameters() {
+        // Do nothing
+    }
+    
+    /**
+     * Instantiate the schema from the given case study name.
+     * 
+     * @param name The case study name
+     * @return The schema instance
+     */
+    private Schema instantiateSchema(String name) {
+        // Get the required schema class
+        try {
+            return (Schema) Class.forName("parsedcasestudy." + name).newInstance();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    private SQLWriter instantiateDBMSWriter(String name) {
+        DBMS dbms = DBMSFactory.instantiate(name);
+        return dbms.getSQLWriter();
+    }
+    
+    /**
+     * Generates mutants of the instantiated schema using the named pipeline.
+     *
+     * @return The mutants
+     */
+    private List<Mutant<Schema>> generateMutants(Schema schema, String dbms) {
+        MutationPipeline<Schema> pipeline;
+        try {
+            pipeline = MutationPipelineFactory.<Schema>instantiate(mutationPipeline, schema, dbms);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
+            throw new RuntimeException(ex);
+        }
+        return pipeline.mutate();
+    }
+
+    private void print(int id, SQLWriter writer, Mutant<Schema> mutant) {
+        System.out.println(id);
+        System.out.println(mutant.getSimpleDescription());
+        System.out.println(mutant.getDescription());
+        List<String> stmts = writer.writeCreateTableStatements(mutant.getMutatedArtefact());
+        for (String stmt : stmts) {
+            System.out.println(stmt);
+        }
+        System.out.println(StringUtils.repeat("-", 30));
+    }
+    
+    private void print(int id, SQLWriter writer, Mutant<Schema> mutant, String file) {
+        try (BufferedWriter output = new BufferedWriter(new FileWriter(file))) {
+            output.write(mutant.getSimpleDescription());
+            output.newLine();
+            output.newLine();
+            output.write(mutant.getDescription());
+            output.newLine();
+            output.newLine();
+            List<String> stmts = writer.writeCreateTableStatements(mutant.getMutatedArtefact());
+            for (String stmt : stmts) {
+                output.write(stmt);
+            }
+            output.newLine();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public static void main(String[] args) {
+        new MutantReporter().run(args);
+    }
+    
+}
