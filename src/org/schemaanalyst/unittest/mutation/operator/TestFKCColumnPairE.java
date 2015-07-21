860a58a9184746c9de50f18fc755d0d199846f9c
Phil McMinn <p.mcminn@sheffield.ac.uk>
2015-07-21 11:43:10 +0100

    Added new tests for FKCColumnPairE

--- src/org/schemaanalyst/unittest/mutation/operator/TestFKCColumnPairE.java
+++ src/org/schemaanalyst/unittest/mutation/operator/TestFKCColumnPairE.java
@@ -17,6 +17,8 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.schemaanalyst.sqlwriter.SQLWriter;
 import org.schemaanalyst.util.tuple.Pair;
 
 public class TestFKCColumnPairE {
@@ -130,21 +132,22 @@ public class TestFKCColumnPairE {
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaNoFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
         assertEquals("Mutating a schema with no foreign keys should produce no "
-                + "mutants", 0, mutants.size());
+                + "mutants when types are on", 0, mutants.size());
 
         exchanger = new FKCColumnPairE(schemaNoFK, false);
         mutants = exchanger.mutate();
         assertEquals("Mutating a schema with no foreign keys should produce no "
-                + "mutants", 0, mutants.size());
+                + "mutants when types are off", 0, mutants.size());
     }
 
     @Test
-    public void testOneColFKey() {
+    public void testOneColFKeyTypesOn() {
         SchemaOneColFK schemaOneColFK = new SchemaOneColFK();
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaOneColFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
-        assertEquals("Mutating a schema with one foreign key should produce 2 "
-                + "mutants", 2, mutants.size());
+        assertEquals("Mutating a schema with one foreign key should produce 4 "
+                + "mutants when types are off", 2, mutants.size());
+
         for (Mutant<Schema> mutant : mutants) {
             Schema schema = mutant.getMutatedArtefact();
             List<ForeignKeyConstraint> fks = schema.getForeignKeyConstraints();
@@ -162,7 +165,31 @@ public class TestFKCColumnPairE {
     }
 
     @Test
-    public void testOneColFKeyMutants() {
+    public void testOneColFKeyTypesOff() {
+        SchemaOneColFK schemaOneColFK = new SchemaOneColFK();
+        FKCColumnPairE exchanger = new FKCColumnPairE(schemaOneColFK, false);
+        List<Mutant<Schema>> mutants = exchanger.mutate();
+        assertEquals("Mutating a schema with one foreign key should produce 2 "
+                + "mutants", 4, mutants.size());
+
+        for (Mutant<Schema> mutant : mutants) {
+            Schema schema = mutant.getMutatedArtefact();
+            List<ForeignKeyConstraint> fks = schema.getForeignKeyConstraints();
+            assertEquals("Mutating a schema with one foreign key constraint "
+                    + "should produce mutants that only contain one foreign "
+                    + "key constraint", 1, fks.size());
+            ForeignKeyConstraint fk = fks.get(0);
+            List<Pair<Column>> pairs = fk.getColumnPairs();
+            assertTrue("Mutating a schema with one foreign key column "
+                    + "should produce mutants that have at most one foreign "
+                    + "key column (the number of columns cannot increase): "
+                    + "\n Value " + pairs.size() + " > 1 for \n"
+                    + mutant.getDescription() + "\n", pairs.size() <= 1);
+        }
+    }
+
+    @Test
+    public void testOneColFKeyMutantsTypesOn() {
         SchemaOneColFK schemaOneColFK = new SchemaOneColFK();
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaOneColFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
@@ -193,7 +220,7 @@ public class TestFKCColumnPairE {
     }
 
     @Test
-    public void testOneColFKeyNoAlternatives() {
+    public void testOneColFKeyNoAlternativesTypesOn() {
         SchemaOneColFKNoAlternatives schemaOneColFK = new SchemaOneColFKNoAlternatives();
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaOneColFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
@@ -202,7 +229,16 @@ public class TestFKCColumnPairE {
     }
 
     @Test
-    public void testTwoColFKey() {
+    public void testOneColFKeyAlternativesTypesOff() {
+        SchemaOneColFKNoAlternatives schemaOneColFK = new SchemaOneColFKNoAlternatives();
+        FKCColumnPairE exchanger = new FKCColumnPairE(schemaOneColFK, false);
+        List<Mutant<Schema>> mutants = exchanger.mutate();
+        assertEquals("Mutating a schema with one foreign key but no alternative "
+               + "columns should produce 4 mutants", 4, mutants.size());
+    }
+
+    @Test
+    public void testTwoColFKeyTypesOn() {
         SchemaTwoColFK schemaTwoColFK = new SchemaTwoColFK();
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaTwoColFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
@@ -225,7 +261,30 @@ public class TestFKCColumnPairE {
     }
 
     @Test
-    public void testTwoColFKeyMutants() {
+    public void testTwoColFKeyTypesOff() {
+        SchemaTwoColFK schemaTwoColFK = new SchemaTwoColFK();
+        FKCColumnPairE exchanger = new FKCColumnPairE(schemaTwoColFK, false);
+        List<Mutant<Schema>> mutants = exchanger.mutate();
+        assertEquals("Mutating a schema with two foreign key columns should "
+                + "produce 6 mutants when types are off", 6, mutants.size());
+        for (Mutant<Schema> mutant : mutants) {
+            Schema schema = mutant.getMutatedArtefact();
+            List<ForeignKeyConstraint> fks = schema.getForeignKeyConstraints();
+            assertEquals("Mutating a schema with one foreign key constraint "
+                    + "should produce mutants that only contain one foreign "
+                    + "key constraint", 1, fks.size());
+            ForeignKeyConstraint fk = fks.get(0);
+            List<Pair<Column>> pairs = fk.getColumnPairs();
+            assertTrue("Mutating a schema with two foreign key columns "
+                    + "should produce mutants that have at most two foreign "
+                    + "key columns (the number of columns cannot increase): "
+                    + "\n Value " + pairs.size() + " > 2 for \n"
+                    + mutant.getDescription() + "\n", pairs.size() <= 2);
+        }
+    }
+
+    @Test
+    public void testTwoColFKeyMutantsTypesOn() {
         SchemaTwoColFK schemaTwoColFK = new SchemaTwoColFK();
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaTwoColFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
@@ -256,16 +315,25 @@ public class TestFKCColumnPairE {
     }
 
     @Test
-    public void SchemaThreeColFKNoAlternatives() {
+    public void testSchemaThreeColFKNoAlternativesTypesOn() {
         SchemaThreeColFKNoAlternatives schemaThreeColFK = new SchemaThreeColFKNoAlternatives();
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaThreeColFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
         assertEquals("Mutating a schema with three foreign key columns but no "
-                + "alternative columns should produce 2 mutants", 2, mutants.size());
+                + "alternative columns should produce 2 mutants when types are on", 2, mutants.size());
     }
 
     @Test
-    public void SchemaThreeColFK() {
+    public void testSchemaThreeColFKNoAlternativesTypesOff() {
+        SchemaThreeColFKNoAlternatives schemaThreeColFK = new SchemaThreeColFKNoAlternatives();
+        FKCColumnPairE exchanger = new FKCColumnPairE(schemaThreeColFK, false);
+        List<Mutant<Schema>> mutants = exchanger.mutate();
+        assertEquals("Mutating a schema with three foreign key columns "
+                + "alternative columns should produce 6 mutants when types are off", 6, mutants.size());
+    }
+
+    @Test
+    public void testSchemaThreeColFKTypesOn() {
         SchemaThreeColFK schemaThreeColFK = new SchemaThreeColFK();
         FKCColumnPairE exchanger = new FKCColumnPairE(schemaThreeColFK, true);
         List<Mutant<Schema>> mutants = exchanger.mutate();
@@ -273,4 +341,17 @@ public class TestFKCColumnPairE {
                 + "produce 6 mutants", 6, mutants.size());
     }
 
+    @Test
+    public void testSchemaThreeColFK() {
+        SchemaThreeColFK schemaThreeColFK = new SchemaThreeColFK();
+        FKCColumnPairE exchanger = new FKCColumnPairE(schemaThreeColFK, false);
+        List<Mutant<Schema>> mutants = exchanger.mutate();
+        assertEquals("Mutating a schema with three foreign key columns should "
+                + "produce 6 mutants when types are on", 6, mutants.size());
+
+        exchanger = new FKCColumnPairE(schemaThreeColFK, false);
+        mutants = exchanger.mutate();
+        assertEquals("Mutating a schema with three foreign key columns should "
+                + "produce 6 mutants when types are off", 6, mutants.size());
+    }
 }
