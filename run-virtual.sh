e3e32bf1f0f46013091c99f29a0a147e68309b74
Chris Wright <aca08cw@sheffield.ac.uk>
2015-11-21 16:28:50 +0000

    Updating parsedcasestudy.Test to highlight failing cases of SQLite and Virtual mutation technique. Including virtual mutation script for ease of execution.

---
+++ run-virtual.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CLASSPATH='lib/*:build/'
+VIRTCLASS='org.schemaanalyst.mutation.analysis.executor.MutationAnalysisVirtual'
+DATAGENERATOR='avsDefaults'
+
+while getopts s:c:r:b: option
+do
+	case "${option}"
+		in
+			s) SCHEMAS=${OPTARG};;
+			c) CRITERION=${OPTARG};;
+			r) REPEATS=${OPTARG};;
+			b) BEGINNING=${OPTARG};;
+	esac
+done
+
+if [ -z $SCHEMAS ] || [ -z $CRITERION ] || [ -z $REPEATS ] ; then
+	echo "Experiment failed - requires -s SCHEMAS -c CRITERION -r REPEATS -b BEGINNING_SEED"
+	exit 1
+fi
+
+IFS=':' read -ra CRITERIA <<< "$CRITERION"
+IFS=':' read -ra SCHEMA <<< "$SCHEMAS"
+
+for (( x=1; x<=$REPEATS; x++ )) do
+	SEED=$(($BEGINNING+$x-1))
+	for s in "${SCHEMA[@]}"; do
+		for c in "${CRITERIA[@]}"; do
+			echo "$x,virtual,$s,$c,false"
+			java -cp $CLASSPATH $VIRTCLASS parsedcasestudy.$s --criterion=$c --randomseed=$SEED --dataGenerator=$DATAGENERATOR
+		done
+	done
+done
+
--- src/parsedcasestudy/Test.java
+++ src/parsedcasestudy/Test.java
@@ -1,26 +1,122 @@
-
 package parsedcasestudy;
 
 import org.schemaanalyst.sqlrepresentation.Schema;
 import org.schemaanalyst.sqlrepresentation.Table;
-import org.schemaanalyst.sqlrepresentation.datatype.IntDataType;
+import org.schemaanalyst.sqlrepresentation.datatype.*;
 
-/**
- *
- * @author Chris J. Wright
+/*
+ * Test schema.
  */
+
+@SuppressWarnings("serial")
 public class Test extends Schema {
 
-    public Test() {
-        super("test");
-        Table one = createTable("one");
-        one.createColumn("a", new IntDataType());
-        createPrimaryKeyConstraint(one, one.getColumn("a"));
-        
-        Table two = createTable("two");
-        two.createColumn("a", new IntDataType());
-//        createForeignKeyConstraint(two, two.getColumn("a"), one, one.getColumn("a"));
-//        createCheckConstraint(two, new NullExpression(new ColumnExpression(two, two.getColumn("a")), true));
-    }
-    
+	// Minimal not-working example
+	// Fails with exception:
+	// "org.schemaanalyst.testgeneration.coveragecriterion.predicate.checker.CheckerException: ''(type + StringValue) is of a different type to 0(type + NumericValue)"
+	public Test() {
+		super("Test");
+		Table tableA = this.createTable("TableA");
+		tableA.createColumn("a", new IntDataType());
+		this.createPrimaryKeyConstraint(tableA, tableA.getColumn("a"));
+		Table tableB = this.createTable("TableB");
+		tableB.createColumn("x", new IntDataType());
+		tableB.createColumn("y", new VarCharDataType(10));
+		this.createForeignKeyConstraint(tableB, tableB.getColumn("x"), tableA, tableA.getColumn("a"));
+	}
+
+	// Replacing type of column y with IntDataType
+	// Successful execution
+	// public Test() {
+	// 	super("Test");
+	// 	Table tableA = this.createTable("TableA");
+	// 	tableA.createColumn("a", new IntDataType());
+	// 	this.createPrimaryKeyConstraint(tableA, tableA.getColumn("a"));
+	// 	Table tableB = this.createTable("TableB");
+	// 	tableB.createColumn("x", new IntDataType());
+	// 	tableB.createColumn("y", new IntDataType()); // VarCharDataType -> IntDataType
+	// 	this.createForeignKeyConstraint(tableB, tableB.getColumn("x"), tableA, tableA.getColumn("a"));
+	// }
+
+	// Replacing type of column y with CharDataType
+	// Fails with exception:
+	// "org.schemaanalyst.testgeneration.coveragecriterion.predicate.checker.CheckerException: ''(type + StringValue) is of a different type to 0(type + NumericValue)"
+	// public Test() {
+	// 	super("Test");
+	// 	Table tableA = this.createTable("TableA");
+	// 	tableA.createColumn("a", new IntDataType());
+	// 	this.createPrimaryKeyConstraint(tableA, tableA.getColumn("a"));
+	// 	Table tableB = this.createTable("TableB");
+	// 	tableB.createColumn("x", new IntDataType());
+	// 	tableB.createColumn("y", new CharDataType(5)); // VarCharDataType -> CharDataType
+	// 	this.createForeignKeyConstraint(tableB, tableB.getColumn("x"), tableA, tableA.getColumn("a"));
+	// }
+
+	// Replacing type of column y with DateDataType
+	// Fails with exception:
+	// "org.schemaanalyst.testgeneration.coveragecriterion.predicate.checker.CheckerException: '2000-01-01'(type + DateValue) is of a different type to 0(type + NumericValue)"
+	// public Test() {
+	// 	super("Test");
+	// 	Table tableA = this.createTable("TableA");
+	// 	tableA.createColumn("a", new IntDataType());
+	// 	this.createPrimaryKeyConstraint(tableA, tableA.getColumn("a"));
+	// 	Table tableB = this.createTable("TableB");
+	// 	tableB.createColumn("x", new IntDataType());
+	// 	tableB.createColumn("y", new DateDataType()); // VarCharDataType -> DateDataType
+	// 	this.createForeignKeyConstraint(tableB, tableB.getColumn("x"), tableA, tableA.getColumn("a"));
+	// }
+
+	// Replacing a and x types with VarCharDataType (needed for FK type alignment
+	// Successful execution
+	// public Test() {
+	// 	super("Test");
+	// 	Table tableA = this.createTable("TableA");
+	// 	tableA.createColumn("a", new VarCharDataType(10));
+	// 	this.createPrimaryKeyConstraint(tableA, tableA.getColumn("a"));
+	// 	Table tableB = this.createTable("TableB");
+	// 	tableB.createColumn("x", new VarCharDataType(10));
+	// 	tableB.createColumn("y", new VarCharDataType(10));
+	// 	this.createForeignKeyConstraint(tableB, tableB.getColumn("x"), tableA, tableA.getColumn("a"));
+	// }
+
+	// Removing column y
+	// Successful execution
+	// public Test() {
+	// 	super("Test");
+	// 	Table tableA = this.createTable("TableA");
+	// 	tableA.createColumn("a", new IntDataType());
+	// 	this.createPrimaryKeyConstraint(tableA, tableA.getColumn("a"));
+	// 	Table tableB = this.createTable("TableB");
+	// 	tableB.createColumn("x", new IntDataType());
+	// 	// Removed column y
+	// 	this.createForeignKeyConstraint(tableB, tableB.getColumn("x"), tableA, tableA.getColumn("a"));
+	// }
+
+	// Removing FK constraint
+	// Successful execution
+	// public Test() {
+	// 	super("Test");
+	// 	Table tableA = this.createTable("TableA");
+	// 	tableA.createColumn("a", new IntDataType());
+	// 	this.createPrimaryKeyConstraint(tableA, tableA.getColumn("a"));
+	// 	Table tableB = this.createTable("TableB");
+	// 	tableB.createColumn("x", new IntDataType());
+	// 	tableB.createColumn("y", new VarCharDataType(10));
+	// 	// Removed FK constraint
+	// }
+
+	// Removing PK constraint (not certain this is a problem)
+	// Fails with exception:
+	// "org.schemaanalyst.testgeneration.TestCaseExecutionException: INSERT statement for setting database state "INSERT INTO "TableB"("x", "y") VALUES (0, '')" should affect exactly one row, was 0""
+	// public Test() {
+	// 	super("Test");
+	// 	Table tableA = this.createTable("TableA");
+	// 	tableA.createColumn("a", new IntDataType());
+	// 	// Removed PK constraint
+	// 	Table tableB = this.createTable("TableB");
+	// 	tableB.createColumn("x", new IntDataType());
+	// 	tableB.createColumn("y", new VarCharDataType(10));
+	// 	this.createForeignKeyConstraint(tableB, tableB.getColumn("x"), tableA, tableA.getColumn("a"));
+	// }
 }
+
