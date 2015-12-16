a4e036d6ef3763402c179639d7dde7c7b0495ada
Chris Wright <aca08cw@sheffield.ac.uk>
2015-12-16 17:47:41 +0000

    Adding support in normaliser to replace 'x IS NOT NULL and y IS NOT NULL' in check constraint with NOT NULL constraints for x and y.

--- src/org/schemaanalyst/dbms/sqlite/SQLiteSchemaNormaliser.java
+++ src/org/schemaanalyst/dbms/sqlite/SQLiteSchemaNormaliser.java
@@ -1,5 +1,7 @@
 package org.schemaanalyst.dbms.sqlite;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.schemaanalyst.mutation.equivalence.SchemaNormaliser;
 import org.schemaanalyst.sqlrepresentation.Column;
 import org.schemaanalyst.sqlrepresentation.Schema;
@@ -7,9 +9,12 @@ import org.schemaanalyst.sqlrepresentation.constraint.CheckConstraint;
 import org.schemaanalyst.sqlrepresentation.constraint.NotNullConstraint;
 import org.schemaanalyst.sqlrepresentation.constraint.PrimaryKeyConstraint;
 import org.schemaanalyst.sqlrepresentation.constraint.UniqueConstraint;
+import org.schemaanalyst.sqlrepresentation.expression.AndExpression;
 import org.schemaanalyst.sqlrepresentation.expression.ColumnExpression;
 import org.schemaanalyst.sqlrepresentation.expression.Expression;
+import org.schemaanalyst.sqlrepresentation.expression.ExpressionAdapter;
 import org.schemaanalyst.sqlrepresentation.expression.NullExpression;
+import org.schemaanalyst.sqlrepresentation.expression.ParenthesisedExpression;
 
 /**
  *
@@ -24,9 +29,10 @@ public class SQLiteSchemaNormaliser extends SchemaNormaliser {
         normaliseCheckNotNull(duplicate);
         return duplicate;
     }
-    
+
     /**
      * Replace each Primary Key with a Unique
+     *
      * @param schema The schema to normalise
      */
     private void normalisePrimaryKeys(Schema schema) {
@@ -39,36 +45,89 @@ public class SQLiteSchemaNormaliser extends SchemaNormaliser {
             schema.removePrimaryKeyConstraint(pk.getTable());
         }
     }
-    
+
     /**
-     * Replace each Check is not null with a not null
+     * Replace each Check is not null with a not null, including those in ANDs
+     *
      * @param schema The schema to normalise
      */
-    private void normaliseCheckNotNull(Schema schema) {
-        for (CheckConstraint check : schema.getCheckConstraints()) {
-            Expression expr = check.getExpression();
-            // Only apply if it is "NULL" expression
-            if (expr instanceof NullExpression) {
-                 NullExpression nullExp = (NullExpression) expr;
-                 // Only apply if it is "NOT NULL" variant
-                 if (nullExp.isNotNull()) {
-                     Expression subexpression = nullExp.getSubexpression();
-                     // Only apply if constraint refers to a column
-                     if (subexpression instanceof ColumnExpression) {
-                         ColumnExpression colExpr = (ColumnExpression) subexpression;
-                         Column col = colExpr.getColumn();
-                         // Remove the check constraint
-                         schema.removeCheckConstraint(check);
-                         // Create an equivalent not null, if one doesn't already exist
-                         NotNullConstraint constraint = new NotNullConstraint(check.getTable(), col);
-                         if (!schema.getNotNullConstraints(check.getTable()).contains(constraint)) {
-                             schema.addNotNullConstraint(constraint);
-                         }
-                     }
-                 }
-                 
-            }
+    private void normaliseCheckNotNull(final Schema schema) {
+        for (final CheckConstraint check : schema.getCheckConstraints()) {
+            ExpressionAdapter visitor = new ExpressionAdapter() {
+                @Override
+                public void visit(AndExpression expression) {
+                    // For any subexpression that is a check is not null expression, replace with not null
+                    List<Expression> newSubexpressions = new ArrayList<>();
+                    for (Expression subexpression : expression.getSubexpressions()) {
+                        subexpression = stripParenthesised(subexpression);
+                        if (subexpression instanceof NullExpression) {
+                            boolean success = replaceNullExpression((NullExpression) subexpression);
+                            if (!success) {
+                                newSubexpressions.add(subexpression);
+                            }
+                        } else {
+                            newSubexpressions.add(subexpression);
+                        }
+                    }
+                    if (newSubexpressions.isEmpty()) {
+                        // Remove if no subexpressions left
+                        schema.removeCheckConstraint(check);
+                    } else {
+                        // Else set the new subexpressions
+                        expression.setSubexpressions(newSubexpressions);
+                    }
+                }
+
+                @Override
+                public void visit(ParenthesisedExpression expression) {
+                    expression.getSubexpression().accept(this);
+                }
+
+                @Override
+                public void visit(NullExpression expression) {
+                    // Replace Check is not null with a not null
+                    boolean success = replaceNullExpression(expression);
+                    if (success) {
+                        // Remove the check constraint
+                        schema.removeCheckConstraint(check);
+                    }
+                }
+
+                /**
+                 * Attempt to replace a NullExpression with a Not Null constraint
+                 * @param expression The NullExpression
+                 * @return Whether it was able to be removed
+                 */
+                private boolean replaceNullExpression(NullExpression expression) {
+                    // Only apply if it is "NOT NULL" variant
+                    boolean success = false;
+                    if (expression.isNotNull()) {
+                        Expression subexpression = expression.getSubexpression();
+                        // Only apply if constraint refers to a column
+                        if (subexpression instanceof ColumnExpression) {
+                            success = true;
+                            ColumnExpression colExpr = (ColumnExpression) subexpression;
+                            Column col = colExpr.getColumn();
+                            // Create an equivalent not null, if one doesn't already exist
+                            NotNullConstraint constraint = new NotNullConstraint(check.getTable(), col);
+                            if (!schema.getNotNullConstraints(check.getTable()).contains(constraint)) {
+                                schema.addNotNullConstraint(constraint);
+                            }
+                        }
+                    }
+                    return success;
+                }
+                
+                private Expression stripParenthesised(Expression expression) {
+                    if (expression instanceof ParenthesisedExpression) {
+                        return ((ParenthesisedExpression)expression).getSubexpression();
+                    } else {
+                        return expression;
+                    }
+                }
+            };
+            check.getExpression().accept(visitor);
         }
     }
-    
+
 }
