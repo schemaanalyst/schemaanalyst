bb9f07064ed973babe0c6da8af0296967fc112bd
Phil McMinn <p.mcminn@sheffield.ac.uk>
2015-12-19 17:50:32 +0000

    Done 20

--- src/paper/ineffectivemutants/manualevaluation/complete/NistXTS749_SQLite_31_NORMAL.java
+++ src/paper/ineffectivemutants/manualevaluation/complete/NistXTS749_SQLite_31_NORMAL.java
@@ -1,4 +1,4 @@
-package paper.ineffectivemutants.manualevaluation.todo;
+package paper.ineffectivemutants.manualevaluation.complete;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -9,9 +9,9 @@ import paper.ineffectivemutants.manualevaluation.ManualAnalysisTestSuite;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
-public class NistXTS749_SQLite_31 extends ManualAnalysisTestSuite {
+public class NistXTS749_SQLite_31_NORMAL extends ManualAnalysisTestSuite {
 	
 	@BeforeClass
 	public static void initialise() throws ClassNotFoundException, SQLException {
@@ -58,25 +58,25 @@ public class NistXTS749_SQLite_31 extends ManualAnalysisTestSuite {
 	/*** BEGIN MANUAL ANALYSIS ***/
 	/*****************************/
 
-	// String statement1 = "INSERT INTO \"\" VALUES( )";
-
+	String statement1 = "INSERT INTO \"STAFF\" VALUES(1, 'A', 1, 1)";
+	String statement2 = "INSERT INTO \"STAFF\" VALUES(NULL, 'A', 1, 1)";
 
 	@Test
 	public void notImpaired() throws SQLException {
-	    // assertTrue(insertToMutant(statement1));
+	    assertTrue(insertToMutant(statement1));
 	}
 
 	@Test
 	public void notEquivalent() throws SQLException {
-	    // assertTrue(originalAndMutantHaveDifferentBehavior(statement1));
+	    assertTrue(originalAndMutantHaveDifferentBehavior(statement2));
 	}
 
 	@Test
 	public void notRedundant() throws SQLException {
-	    // assertEquals(mutantAndOtherMutantsHaveDifferentBehavior(statement1), SUCCESS);
+	    assertEquals(mutantAndOtherMutantsHaveDifferentBehavior(statement2), SUCCESS);
 	}
 
-	// ENTER END VERDICT (delete as appropriate): impaired/equivalent/redundant/normal
+	// ENTER END VERDICT (delete as appropriate): normal
 
 	/*****************************/
 	/***  END MANUAL ANALYSIS  ***/
---
+++ src/paper/ineffectivemutants/manualevaluation/todo/NistDML182_SQLite_245.java
@@ -0,0 +1,85 @@
+package paper.ineffectivemutants.manualevaluation.todo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import paper.ineffectivemutants.manualevaluation.ManualAnalysisTestSuite;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class NistDML182_SQLite_245 extends ManualAnalysisTestSuite {
+	
+	@BeforeClass
+	public static void initialise() throws ClassNotFoundException, SQLException {
+		// load the JDBC driver and create the connection and statement object used by this test suite
+		Class.forName("org.sqlite.JDBC");
+		connection = DriverManager.getConnection("jdbc:sqlite:manualanalysis");
+
+		// create the statement
+		statement = connection.createStatement();
+
+		// enable FOREIGN KEY support
+		statement.executeUpdate("PRAGMA foreign_keys = ON");
+	}
+	
+	@AfterClass
+	public static void close() throws SQLException {
+		if (connection != null) {
+			connection.close();
+		}
+	}
+	protected String getSchemaName() {
+	    return "NistDML182";
+	}
+	
+	protected String getDBMSName() {
+	    return "SQLite";
+	}
+	
+	protected int getMutantNumberBeingEvaluated() {
+	    return 245;
+	}
+	
+	protected int getLastMutantNumber() {
+	    return 351;
+	}
+	
+	@After
+	public void dropTables() throws SQLException {
+		statement.executeUpdate("DROP TABLE IF EXISTS \"ORDERS\"");
+		statement.executeUpdate("DROP TABLE IF EXISTS \"ID_CODES\"");
+	}
+
+	/*****************************/
+	/*** BEGIN MANUAL ANALYSIS ***/
+	/*****************************/
+
+	String statement1 = "INSERT INTO \"\" VALUES( )";
+
+	@Test
+	public void notImpaired() throws SQLException {
+	    assertTrue(insertToMutant(statement1));
+	}
+
+	@Test
+	public void notEquivalent() throws SQLException {
+	    assertTrue(originalAndMutantHaveDifferentBehavior(statement1));
+	}
+
+	@Test
+	public void notRedundant() throws SQLException {
+	    assertEquals(mutantAndOtherMutantsHaveDifferentBehavior(statement1), SUCCESS);
+	}
+
+	// ENTER END VERDICT (delete as appropriate): impaired/equivalent/redundant/normal
+
+	/*****************************/
+	/***  END MANUAL ANALYSIS  ***/
+	/*****************************/
+
+}
+
