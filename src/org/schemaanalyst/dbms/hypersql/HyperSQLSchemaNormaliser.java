025e0a84ac16966bd0f8f820520bed67f7302b65
Chris Wright <aca08cw@sheffield.ac.uk>
2015-12-20 08:43:04 +0000

    Adding NormaliseUniquesToMostConstrainedUniques to Postgres, HyperSQL and SQLite DBMS normalisers

--- src/org/schemaanalyst/dbms/hypersql/HyperSQLSchemaNormaliser.java
+++ src/org/schemaanalyst/dbms/hypersql/HyperSQLSchemaNormaliser.java
@@ -2,6 +2,7 @@ package org.schemaanalyst.dbms.hypersql;
 
 import org.schemaanalyst.mutation.normalisation.NormaliseCheckIsNotNullToNotNull;
 import org.schemaanalyst.mutation.normalisation.NormalisePrimaryKeyToUniqueAndNotNull;
+import org.schemaanalyst.mutation.normalisation.NormaliseUniquesToMostConstrainedUniques;
 import org.schemaanalyst.mutation.normalisation.SchemaNormaliser;
 import org.schemaanalyst.sqlrepresentation.Schema;
 
@@ -14,6 +15,8 @@ public class HyperSQLSchemaNormaliser extends SchemaNormaliser {
 
     private static final SchemaNormaliser PRIMARY_KEY_TO_UNIQUE_AND_NOT_NULL = 
             new NormalisePrimaryKeyToUniqueAndNotNull();
+    private static final SchemaNormaliser UNIQUES_TO_MOST_CONSTRAINED_UNIQUES =
+            new NormaliseUniquesToMostConstrainedUniques();
     private static final SchemaNormaliser CHECK_IS_NOT_NULL_TO_NOT_NULL =
             new NormaliseCheckIsNotNullToNotNull();
     
@@ -21,6 +24,7 @@ public class HyperSQLSchemaNormaliser extends SchemaNormaliser {
     public Schema normalise(Schema schema) {
         Schema duplicate = schema.duplicate();
         PRIMARY_KEY_TO_UNIQUE_AND_NOT_NULL.normalise(duplicate);
+        UNIQUES_TO_MOST_CONSTRAINED_UNIQUES.normalise(duplicate);
         CHECK_IS_NOT_NULL_TO_NOT_NULL.normalise(duplicate);
         return duplicate;
     }
--- src/org/schemaanalyst/dbms/sqlite/SQLiteSchemaNormaliser.java
+++ src/org/schemaanalyst/dbms/sqlite/SQLiteSchemaNormaliser.java
@@ -2,6 +2,7 @@ package org.schemaanalyst.dbms.sqlite;
 
 import org.schemaanalyst.mutation.normalisation.NormaliseCheckIsNotNullToNotNull;
 import org.schemaanalyst.mutation.normalisation.NormalisePrimaryKeyToUnique;
+import org.schemaanalyst.mutation.normalisation.NormaliseUniquesToMostConstrainedUniques;
 import org.schemaanalyst.mutation.normalisation.SchemaNormaliser;
 import org.schemaanalyst.sqlrepresentation.Schema;
 
@@ -14,6 +15,8 @@ public class SQLiteSchemaNormaliser extends SchemaNormaliser {
 
     private static final SchemaNormaliser PRIMARY_KEY_TO_UNIQUE = 
             new NormalisePrimaryKeyToUnique();
+    private static final SchemaNormaliser UNIQUES_TO_MOST_CONSTRAINED_UNIQUES =
+            new NormaliseUniquesToMostConstrainedUniques();
     private static final SchemaNormaliser CHECK_IS_NOT_NULL_TO_NOT_NULL =
             new NormaliseCheckIsNotNullToNotNull();
     
@@ -21,6 +24,7 @@ public class SQLiteSchemaNormaliser extends SchemaNormaliser {
     public Schema normalise(Schema schema) {
         Schema duplicate = schema.duplicate();
         PRIMARY_KEY_TO_UNIQUE.normalise(duplicate);
+        UNIQUES_TO_MOST_CONSTRAINED_UNIQUES.normalise(duplicate);
         CHECK_IS_NOT_NULL_TO_NOT_NULL.normalise(duplicate);
         return duplicate;
     }
